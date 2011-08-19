403b026bf616934b9784bf078269dd024a718893
Corey Johnson <cj@github.com>
2011-08-19 09:48:08 -0700

    displays error when coffee-script is not installed

--- script/compile.sh
+++ script/compile.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+hash coffee 2>&- || { echo >&2 "error: Coffee is required but it's not installed (http://jashkenas.github.com/coffee-script/)."; exit 1; }
+
 PATH="$PATH:/usr/local/bin/"
 coffee -o "$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/Resources/" HTML/*.coffee
