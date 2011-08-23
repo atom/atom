3d8545bbb071d71636b080f93e770c97aaab5e40
Corey Johnson <cj@github.com>
2011-08-23 16:20:12 -0700

    just keep all the compile/mv stuff in one script

--- script/compile.sh
+++ script/compile.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+SOURCE_SCRIPTS_DIR="$PROJECT_DIR/HTML"
+DESTINATION_SCRIPTS_DIR="$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/Resources/"
+
 PATH="$PATH:/usr/local/bin/"
 hash coffee 2>&- || { echo >&2 "error: Coffee is required but it's not installed (http://jashkenas.github.com/coffee-script/)."; exit 1; }
-coffee -o "$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/Resources/HTML" HTML/*.coffee
+coffee -o "$DESTINATION_SCRIPTS_DIR/HTML/" HTML/*.coffee
+
+cp -r "$SOURCE_SCRIPTS_DIR" "$DESTINATION_SCRIPTS_DIR"
