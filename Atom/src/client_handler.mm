ac61e18f2c60c038126429b1e9f49b1172dc6d3c
Corey Johnson <cj@github.com>
2012-03-14 09:51:58 -0700

    Format console log better

--- Atom/src/client_handler.mm
+++ Atom/src/client_handler.mm
@@ -211,7 +211,13 @@ bool ClientHandler::OnConsoleMessage(CefRefPtr<CefBrowser> browser,
 {
   REQUIRE_UI_THREAD();
 
-  std::cout << std::string(message) << "\n";
+  std::cout << 
+    std::string(message) <<
+    "\n\t" <<
+    std::string(source) << 
+    ":" << 
+    line << 
+    "\n";
   
   return true;
 }
