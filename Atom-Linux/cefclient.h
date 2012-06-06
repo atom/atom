f269b1f12e41cf7124ec966862e8975f895bea5c
Kevin Sawicki <kevin@github.com>
2012-06-06 11:43:27 -0700

    Remove unused test methods

--- Atom-Linux/cefclient.h
+++ Atom-Linux/cefclient.h
@@ -29,7 +29,7 @@ std::string AppPath();
 std::string PathToOpen();
 
 // Initialize the application command line.
-void AppInitCommandLine(int argc, const char* const* argv);
+void AppInitCommandLine(int argc, const char* const * argv);
 
 // Returns the application command line object.
 CefRefPtr<CefCommandLine> AppGetCommandLine();
@@ -39,29 +39,5 @@ void AppGetSettings(CefSettings& settings, CefRefPtr<CefApp>& app);
 
 // Returns the application browser settings based on command line arguments.
 void AppGetBrowserSettings(CefBrowserSettings& settings);
-
-// Implementations for various tests.
-void RunGetSourceTest(CefRefPtr<CefBrowser> browser);
-void RunGetTextTest(CefRefPtr<CefBrowser> browser);
-void RunRequestTest(CefRefPtr<CefBrowser> browser);
-void RunJavaScriptExecuteTest(CefRefPtr<CefBrowser> browser);
-void RunJavaScriptInvokeTest(CefRefPtr<CefBrowser> browser);
-void RunPopupTest(CefRefPtr<CefBrowser> browser);
-void RunLocalStorageTest(CefRefPtr<CefBrowser> browser);
-void RunAccelerated2DCanvasTest(CefRefPtr<CefBrowser> browser);
-void RunAcceleratedLayersTest(CefRefPtr<CefBrowser> browser);
-void RunWebGLTest(CefRefPtr<CefBrowser> browser);
-void RunHTML5VideoTest(CefRefPtr<CefBrowser> browser);
-void RunXMLHTTPRequestTest(CefRefPtr<CefBrowser> browser);
-void RunWebURLRequestTest(CefRefPtr<CefBrowser> browser);
-void RunDOMAccessTest(CefRefPtr<CefBrowser> browser);
-void RunDragDropTest(CefRefPtr<CefBrowser> browser);
-void RunModalDialogTest(CefRefPtr<CefBrowser> browser);
-void RunPluginInfoTest(CefRefPtr<CefBrowser> browser);
-
-#if defined(OS_WIN)
-void RunTransparentPopupTest(CefRefPtr<CefBrowser> browser);
-void RunGetImageTest(CefRefPtr<CefBrowser> browser);
-#endif
 
 #endif  // CEF_TESTS_CEFCLIENT_CEFCLIENT_H_
