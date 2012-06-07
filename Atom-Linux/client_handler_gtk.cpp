deaad13fc22407aea84b111b3cd0d4a015227c46
Kevin Sawicki <kevin@github.com>
2012-06-07 13:45:42 -0700

    Format client_handler_gtk.cpp
    No functional change

--- Atom-Linux/client_handler_gtk.cpp
+++ Atom-Linux/client_handler_gtk.cpp
@@ -11,62 +11,58 @@
 
 // ClientHandler::ClientLifeSpanHandler implementation
 bool ClientHandler::OnBeforePopup(CefRefPtr<CefBrowser> parentBrowser,
-                                  const CefPopupFeatures& popupFeatures,
-                                  CefWindowInfo& windowInfo,
-                                  const CefString& url,
-                                  CefRefPtr<CefClient>& client,
-                                  CefBrowserSettings& settings) {
-  REQUIRE_UI_THREAD();
+		const CefPopupFeatures& popupFeatures, CefWindowInfo& windowInfo,
+		const CefString& url, CefRefPtr<CefClient>& client,
+		CefBrowserSettings& settings) {
+	REQUIRE_UI_THREAD();
 
-  return false;
+	return false;
 }
 
 void ClientHandler::OnAddressChange(CefRefPtr<CefBrowser> browser,
-                                    CefRefPtr<CefFrame> frame,
-                                    const CefString& url) {
-  REQUIRE_UI_THREAD();
+		CefRefPtr<CefFrame> frame, const CefString& url) {
+	REQUIRE_UI_THREAD();
 
-  if (m_BrowserHwnd == browser->GetWindowHandle() && frame->IsMain()) {
-      // Set the edit window text
-    std::string urlStr(url);
-      gtk_entry_set_text(GTK_ENTRY(m_EditHwnd), urlStr.c_str());
-  }
+	if (m_BrowserHwnd == browser->GetWindowHandle() && frame->IsMain()) {
+		// Set the edit window text
+		std::string urlStr(url);
+		gtk_entry_set_text(GTK_ENTRY(m_EditHwnd), urlStr.c_str());
+	}
 }
 
 void ClientHandler::OnTitleChange(CefRefPtr<CefBrowser> browser,
-                                  const CefString& title) {
-  REQUIRE_UI_THREAD();
+		const CefString& title) {
+	REQUIRE_UI_THREAD();
 
-  GtkWidget* window =
-      gtk_widget_get_ancestor(GTK_WIDGET(browser->GetWindowHandle()),
-                              GTK_TYPE_WINDOW);
-  std::string titleStr(title);
-  gtk_window_set_title(GTK_WINDOW(window), titleStr.c_str());
+	GtkWidget* window = gtk_widget_get_ancestor(
+			GTK_WIDGET(browser->GetWindowHandle()), GTK_TYPE_WINDOW);
+	std::string titleStr(title);
+	gtk_window_set_title(GTK_WINDOW(window), titleStr.c_str());
 }
 
 void ClientHandler::SendNotification(NotificationType type) {
-  // TODO(port): Implement this method.
+	// TODO(port): Implement this method.
 }
 
 void ClientHandler::SetLoading(bool isLoading) {
-  if (isLoading)
-    gtk_widget_set_sensitive(GTK_WIDGET(m_StopHwnd), true);
-  else
-    gtk_widget_set_sensitive(GTK_WIDGET(m_StopHwnd), false);
+	if (isLoading)
+		gtk_widget_set_sensitive(GTK_WIDGET(m_StopHwnd), true);
+	else
+		gtk_widget_set_sensitive(GTK_WIDGET(m_StopHwnd), false);
 }
 
 void ClientHandler::SetNavState(bool canGoBack, bool canGoForward) {
-  if (canGoBack)
-    gtk_widget_set_sensitive(GTK_WIDGET(m_BackHwnd), true);
-  else
-    gtk_widget_set_sensitive(GTK_WIDGET(m_BackHwnd), false);
+	if (canGoBack)
+		gtk_widget_set_sensitive(GTK_WIDGET(m_BackHwnd), true);
+	else
+		gtk_widget_set_sensitive(GTK_WIDGET(m_BackHwnd), false);
 
-  if (canGoForward)
-    gtk_widget_set_sensitive(GTK_WIDGET(m_ForwardHwnd), true);
-  else
-    gtk_widget_set_sensitive(GTK_WIDGET(m_ForwardHwnd), false);
+	if (canGoForward)
+		gtk_widget_set_sensitive(GTK_WIDGET(m_ForwardHwnd), true);
+	else
+		gtk_widget_set_sensitive(GTK_WIDGET(m_ForwardHwnd), false);
 }
 
 void ClientHandler::CloseMainWindow() {
-  // TODO(port): Close main window.
+	// TODO(port): Close main window.
 }
