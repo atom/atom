db6230fbdceb5e9e18337695be842fc5c1476c60
Nathan Sobo <nathan@github.com>
2012-08-10 16:08:11 -0600

    Rename run.sh -> build.sh. Add tools directory w/ vendored GYP

---
+++ build.sh
@@ -0,0 +1,1 @@
+python tools/gyp/gyp --depth=. atom.gyp && xcodebuild -project atom.xcodeproj/ -scheme Atom clean build
--- run.sh
+++
@@ -1,1 +0,0 @@
-python ~/code/gyp/gyp --depth=. atom.gyp && xcodebuild -project atom.xcodeproj/ -configuration Debug -target Atom clean build && open build/Default/Atom.app
