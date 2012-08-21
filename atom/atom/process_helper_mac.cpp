cc7b8e443da909c2a6e10d4a3ce7b6e1087f2872
Corey Johnson <cj@github.com>
2012-08-21 13:19:33 -0700

    :shit:

--- atom/atom/process_helper_mac.cpp
+++
@@ -1,7 +0,0 @@
-#include "include/cef_app.h"
-
-int main(int argc, char* argv[]) {
-  CefMainArgs main_args(argc, argv);
-  CefRefPtr<CefApp> app;
-  return CefExecuteProcess(main_args, app); // Execute the secondary process.
-}
