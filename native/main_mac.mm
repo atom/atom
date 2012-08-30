b0096ee0e636b2ca909b859be3075d3a4de83fd1
Corey Johnson <cj@github.com>
2012-08-30 09:46:31 -0700

    Only non-mac versions of the app need to run CefExecuteProcess in their main function.

--- native/main_mac.mm
+++ native/main_mac.mm
@@ -3,15 +3,6 @@
 
 int main(int argc, char* argv[]) {
   @autoreleasepool {
-//    TODO: Ask marshal why we need this?
-//    CefMainArgs mainArgs(argc, argv);
-//    CefRefPtr<CefApp> app;
-//
-//    int exit_code = CefExecuteProcess(mainArgs, app.get());
-//    if (exit_code >= 0){
-//      return exit_code;
-//    }
-
     NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
     AtomApplication *application = (AtomApplication *)[AtomApplication sharedApplication];
 
