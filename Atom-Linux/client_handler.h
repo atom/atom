27d2d14414b796aba61f4f69dfd362947692d5e0
Corey Johnson & Nathan Sobo <cj+nathan@github.com>
2012-08-31 10:43:21 -0700

    Carry CEF1 directories over to CEF3

---
+++ Atom-Linux/client_handler.h
@@ -0,0 +1,155 @@
+#ifndef CLIENT_HANDLER_H_
+#define CLIENT_HANDLER_H_
+#pragma once
+
+#include <map>
+#include <string>
+#include "include/cef_client.h"
+#include "util.h"
+#include "native_handler.h"
+
+// ClientHandler implementation.
+class ClientHandler: public CefClient,
+    public CefLifeSpanHandler,
+    public CefLoadHandler,
+    public CefDisplayHandler,
+    public CefFocusHandler,
+    public CefKeyboardHandler,
+    public CefPrintHandler,
+    public CefV8ContextHandler,
+    public CefDragHandler {
+public:
+  ClientHandler();
+  virtual ~ClientHandler();
+
+  // CefClient methods
+  virtual CefRefPtr<CefLifeSpanHandler> GetLifeSpanHandler() OVERRIDE {
+    return this;
+  }
+  virtual CefRefPtr<CefLoadHandler> GetLoadHandler() OVERRIDE {
+    return this;
+  }
+  virtual CefRefPtr<CefDisplayHandler> GetDisplayHandler() OVERRIDE {
+    return this;
+  }
+  virtual CefRefPtr<CefFocusHandler> GetFocusHandler() OVERRIDE {
+    return this;
+  }
+  virtual CefRefPtr<CefKeyboardHandler> GetKeyboardHandler() OVERRIDE {
+    return this;
+  }
+  virtual CefRefPtr<CefPrintHandler> GetPrintHandler() OVERRIDE {
+    return this;
+  }
+  virtual CefRefPtr<CefV8ContextHandler> GetV8ContextHandler() OVERRIDE {
+    return this;
+  }
+  virtual CefRefPtr<CefDragHandler> GetDragHandler() OVERRIDE {
+    return this;
+  }
+
+  // CefLifeSpanHandler methods
+  virtual bool OnBeforePopup(CefRefPtr<CefBrowser> parentBrowser,
+      const CefPopupFeatures& popupFeatures, CefWindowInfo& windowInfo,
+      const CefString& url, CefRefPtr<CefClient>& client,
+      CefBrowserSettings& settings) OVERRIDE;
+  virtual void OnAfterCreated(CefRefPtr<CefBrowser> browser) OVERRIDE;
+  virtual bool DoClose(CefRefPtr<CefBrowser> browser) OVERRIDE;
+  virtual void OnBeforeClose(CefRefPtr<CefBrowser> browser) OVERRIDE;
+
+  // CefLoadHandler methods
+  virtual void OnLoadStart(CefRefPtr<CefBrowser> browser,
+      CefRefPtr<CefFrame> frame) OVERRIDE;
+  virtual void OnLoadEnd(CefRefPtr<CefBrowser> browser,
+      CefRefPtr<CefFrame> frame, int httpStatusCode) OVERRIDE;
+  virtual bool OnLoadError(CefRefPtr<CefBrowser> browser,
+      CefRefPtr<CefFrame> frame, ErrorCode errorCode,
+      const CefString& failedUrl, CefString& errorText) OVERRIDE;
+
+  // CefDisplayHandler methods
+  virtual void OnNavStateChange(CefRefPtr<CefBrowser> browser, bool canGoBack,
+      bool canGoForward) OVERRIDE;
+  virtual void OnAddressChange(CefRefPtr<CefBrowser> browser,
+      CefRefPtr<CefFrame> frame, const CefString& url) OVERRIDE;
+  virtual void OnTitleChange(CefRefPtr<CefBrowser> browser,
+      const CefString& title) OVERRIDE;
+  virtual bool OnConsoleMessage(CefRefPtr<CefBrowser> browser,
+      const CefString& message, const CefString& source, int line) OVERRIDE;
+
+  // CefFocusHandler methods.
+  virtual void OnFocusedNodeChanged(CefRefPtr<CefBrowser> browser,
+      CefRefPtr<CefFrame> frame, CefRefPtr<CefDOMNode> node) OVERRIDE;
+
+  // CefKeyboardHandler methods.
+  virtual bool OnKeyEvent(CefRefPtr<CefBrowser> browser, KeyEventType type,
+      int code, int modifiers, bool isSystemKey, bool isAfterJavaScript)
+          OVERRIDE;
+
+  // CefPrintHandler methods.
+  virtual bool GetPrintHeaderFooter(CefRefPtr<CefBrowser> browser,
+      CefRefPtr<CefFrame> frame, const CefPrintInfo& printInfo,
+      const CefString& url, const CefString& title, int currentPage,
+      int maxPages, CefString& topLeft, CefString& topCenter,
+      CefString& topRight, CefString& bottomLeft, CefString& bottomCenter,
+      CefString& bottomRight) OVERRIDE;
+
+  // CefV8ContextHandler methods
+  virtual void OnContextCreated(CefRefPtr<CefBrowser> browser,
+      CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context) OVERRIDE;
+
+  // CefDragHandler methods.
+  virtual bool OnDragStart(CefRefPtr<CefBrowser> browser,
+      CefRefPtr<CefDragData> dragData, DragOperationsMask mask) OVERRIDE;
+  virtual bool OnDragEnter(CefRefPtr<CefBrowser> browser,
+      CefRefPtr<CefDragData> dragData, DragOperationsMask mask) OVERRIDE;
+
+  void SetWindow(GtkWidget* window);
+  void SetMainHwnd(CefWindowHandle hwnd);
+  CefWindowHandle GetMainHwnd() {
+    return m_MainHwnd;
+  }
+  void SetEditHwnd(CefWindowHandle hwnd);
+
+  CefRefPtr<CefBrowser> GetBrowser() {
+    return m_Browser;
+  }
+  CefWindowHandle GetBrowserHwnd() {
+    return m_BrowserHwnd;
+  }
+
+  enum NotificationType {
+    NOTIFY_CONSOLE_MESSAGE
+  };
+  void SendNotification(NotificationType type);
+  void CloseMainWindow();
+
+protected:
+
+  GtkWidget* window;
+
+  CefRefPtr<NativeHandler> m_nativeHandler;
+
+  // The child browser window
+  CefRefPtr<CefBrowser> m_Browser;
+
+  // The main frame window handle
+  CefWindowHandle m_MainHwnd;
+
+  // The child browser window handle
+  CefWindowHandle m_BrowserHwnd;
+
+  // The edit window handle
+  CefWindowHandle m_EditHwnd;
+
+  // Support for logging.
+  std::string m_LogFile;
+
+  // Include the default reference counting implementation.
+IMPLEMENT_REFCOUNTING(ClientHandler)
+  ;
+  // Include the default locking implementation.
+IMPLEMENT_LOCKING(ClientHandler)
+  ;
+};
+
+#endif
---
+++ README.md
@@ -0,0 +1,100 @@
+# Atom — Futuristic Text Editing
+## Be forwarned: Atom is pre-alpha software!
+
+
+## Building from source
+
+1. Get [xcode 4.2 or above](http://itunes.apple.com/us/app/xcode/id448457090?mt=12)
+
+2. Install CoffeeScript http://coffeescript.org/
+```
+brew install nodejs
+curl https://npmjs.org/install.sh | sh
+npm i -g coffee-script
+```
+
+3. `git clone git@github.com:github/atom.git`
+
+4. `cd atom`
+
+5. `rake run`
+
+
+## Setup Your ~/.atom Directory
+An example .atom directory can be found at the root of the source tree
+
+## Basic Keyboard shortcuts
+Atom doesn't have much in the way of menus yet. Use these keyboard shortcuts to
+explore features.
+
+`cmd-o` : open file/directory
+
+`cmd-n` : new window
+
+`cmd-t` : open fuzzy file finder
+
+`cmd-:` : open command prompt
+
+`cmd-f` : open command prompt with /
+
+`cmd-g` : repeat the last search
+
+`cmd-r` : reload the current window
+
+`cmd-alt-ctrl-s` : run specs
+
+`cmd-alt-arrows` : split screen in direction of arrow
+
+`cmd-alt-w` : toggle word wrap
+
+`cmd-alt-f` : fold selected lines
+
+Most default OS X keybindings also work.
+
+## Init Script
+
+Atom will require `~/.atom/atom.coffee` whenever a window is opened or reloaded if it is present in your
+home directory. This is a rudimentary jumping off point for your own customizations.
+
+## Command Panel
+
+A partial implementation of the [Sam command language](http://man.cat-v.org/plan_9/1/sam)
+
+*Examples*
+
+`,` selects entire file
+
+`1,4` selects lines 1-4
+
+`/pattern` selects the first match after the cursor/selection
+
+`s/pattern/replacement` replace first text matching pattern in current selection
+
+`s/pattern/replacement/g` replace all text matching pattern in current selection
+
+`,s/pattern/replacement/g` replace all text matching pattern in file
+
+`1,4s/pattern/replacement` replace all text matching pattern in lines 1-4
+
+`x/pattern` selects all matches in the current selections
+
+`,x/pattern` selects all matches in the file
+
+`,x/pattern1/ x/pattern2` "structural regex" - selects all matches of pattern2 inside matches of pattern1
+
+## Key Bindings
+
+Atom has a CSS based key binding scheme. We will add a nicer loading mechanism, but for now you can bind
+keys by calling `window.keymap.bindKeys` with a CSS selector and a hash of key-pattern -> event mappings.
+
+```coffeescript
+window.keymap.bindKeys '.editor'
+  'ctrl-p': 'party-time'
+  'ctrl-q': 'open-dialog-q'
+```
+
+When a keypress matches a pattern on an element that matches the selector, it will be translated to the
+named event, which will bubble up the DOM from the site of the keypress. Extension code can listen for
+the named event and react to it.
+
+
