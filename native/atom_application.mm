5ae185c4f5cd01d272ba103467204937f07eb8da
Corey Johnson & Nathan Sobo <cj+nathan@github.com>
2012-09-12 15:29:01 -0700

    Remove unused code

--- native/atom_application.mm
+++ native/atom_application.mm
@@ -138,11 +138,6 @@
   CefShutdown();
 }
 
-- (BOOL)applicationShouldHandleReopen:(NSApplication *)application hasVisibleWindows:(BOOL)flag {
-  NSLog(@"%@", @"OK OK OK");
-  return YES;
-}
-
 # pragma mark CefAppProtocol
 
 - (BOOL)isHandlingSendEvent {
