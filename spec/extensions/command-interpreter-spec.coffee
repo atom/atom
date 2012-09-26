atom/atom

commit aa3f674948d9502cf204d8ffd2edc7137f08f0ed

Regex's in the command panel are treated as case-insensitive if there are no non-escaped letters in the pattern
/hello becomes /hello/i
/he\Slo becomes /he\Slo/i
/Hello/i becomes /Hello/i
/Hello/ becomes /Hello/

src/extensions/command-panel/commands/regex-address.coffee
@@ -6,9 +6,15 @@ class RegexAddress extends Address
   regex: null
   reverse: null
 
-  constructor: (pattern, isReversed) ->
+  constructor: (pattern, isReversed, options) ->
+    flags = ""
+    pattern = pattern.source if pattern.source
+
+    patternContainsCapitalLetter = /[^\\][A-Z]/.test(pattern)
+    flags += "i" unless patternContainsCapitalLetter
     @isReversed = isReversed
-    @regex = new RegExp(pattern)
+
+    @regex = new RegExp(pattern, flags)
 
   getRange: (buffer, range) ->
     rangeBefore = new Range([0, 0], range.start)

spec/extensions/command-interpreter-spec.coffee
@@ -161,6 +161,22 @@ describe "CommandInterpreter", ->
           expect(editSession.lineForScreenRow(5).fold).toBeUndefined()
           expect(editSession.lineForScreenRow(10).fold).toBeDefined()
 
+      it "is case-insentive when the pattern contains no non-escaped uppercase letters (behavior copied from vim)", ->
+        waitsForPromise ->
+          editSession.setSelectedBufferRange([[4,16], [4,20]])
+          interpreter.eval('/array', editSession)
+
+        runs ->
+          expect(editSession.getSelection().getBufferRange()).toEqual [[11,14], [11,19]]
+
+        waitsForPromise ->
+          editSession.setSelectedBufferRange([[4,16], [4,20]])
+          interpreter.eval('/a\\Sray', editSession) # You must escape the backslash, otherwise it is treated as an escaped 'S'
+
+        runs ->
+          expect(editSession.getSelection().getBufferRange()).toEqual [[11,14], [11,19]]
+
+
     describe "address range", ->
       describe "when two addresses are specified", ->
         it "selects from the begining of the left address to the end of the right address", ->

spec/extensions/command-panel-spec.coffee
@@ -241,7 +241,7 @@ describe "CommandPanel", ->
       expect(commandInterpreter.lastRelativeAddress).toBeUndefined()
       rootView.trigger 'command-panel:set-selection-as-regex-address'
       expect(commandInterpreter.lastRelativeAddress.subcommands.length).toBe 1
-      expect(commandInterpreter.lastRelativeAddress.subcommands[0].regex.toString()).toEqual "/\\(items\\)/"
+      expect(commandInterpreter.lastRelativeAddress.subcommands[0].regex.toString()).toEqual "/\\(items\\)/i"
 
   describe "when command-panel:find-in-file is triggered on an editor", ->
     it "pre-populates the command panel's editor with / and moves the cursor to the last column", ->

commit 8328875021a5a485f92d11da092272c0b844ac2a

Regex searches are case-sensitive if any letter is capitalized.
Fixes bug introduced in commit aa3f674948d9502cf204d8ffd2edc7137f08f0ed

src/extensions/command-panel/commands/regex-address.coffee
@@ -10,7 +10,7 @@ class RegexAddress extends Address
     flags = ""
     pattern = pattern.source if pattern.source
 
-    patternContainsCapitalLetter = /[^\\][A-Z]/.test(pattern)
+    patternContainsCapitalLetter = /(^|[^\\])[A-Z]/.test(pattern)
     flags += "i" unless patternContainsCapitalLetter
     @isReversed = isReversed
 

spec/extensions/command-interpreter-spec.coffee
@@ -163,19 +163,25 @@ describe "CommandInterpreter", ->
 
       it "is case-insentive when the pattern contains no non-escaped uppercase letters (behavior copied from vim)", ->
         waitsForPromise ->
-          editSession.setSelectedBufferRange([[4,16], [4,20]])
           interpreter.eval('/array', editSession)
-
         runs ->
-          expect(editSession.getSelection().getBufferRange()).toEqual [[11,14], [11,19]]
+          expect(interpreter.lastRelativeAddress.subcommands[0].regex.toString()).toEqual "/array/i"
 
         waitsForPromise ->
-          editSession.setSelectedBufferRange([[4,16], [4,20]])
-          interpreter.eval('/a\\Sray', editSession) # You must escape the backslash, otherwise it is treated as an escaped 'S'
-
+          interpreter.eval('/a\\Sray', editSession)
         runs ->
-          expect(editSession.getSelection().getBufferRange()).toEqual [[11,14], [11,19]]
+          expect(interpreter.lastRelativeAddress.subcommands[0].regex.toString()).toEqual "/a\\Sray/i"
 
+      it "is case-sentive when the pattern contains a non-escaped uppercase letters (behavior copied from vim)", ->
+        waitsForPromise ->
+          interpreter.eval('/arRay', editSession)
+        runs ->
+          expect(interpreter.lastRelativeAddress.subcommands[0].regex.toString()).toEqual "/arRay/"
+
+        waitsForPromise ->
+          interpreter.eval('/Array', editSession)
+        runs ->
+          expect(interpreter.lastRelativeAddress.subcommands[0].regex.toString()).toEqual "/Array/"
 
     describe "address range", ->
       describe "when two addresses are specified", ->