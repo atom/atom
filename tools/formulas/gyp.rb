82af5e826c7512cd4d10597790e344e1fbd3a427
Kevin Sawicki <kevin@github.com>
2012-10-09 12:06:27 -0700

    Use specific gyp revision

--- tools/formulas/gyp.rb
+++ tools/formulas/gyp.rb
@@ -4,6 +4,8 @@ require 'formula'
 
 class Gyp < Formula
   homepage 'http://code.google.com/p/gyp/'
+  url 'http://gyp.googlecode.com/svn/trunk', :revision => 1518
+  version '1518'
   head 'http://gyp.googlecode.com/svn/trunk'
 
   def install
