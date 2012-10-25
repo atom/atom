02179be0ef67d1dab0576a2b736c31815d1100f3
Kevin Sawicki <kevin@github.com>
2012-10-24 18:27:19 -0700

    Spec out more $git methods

--- native/v8_extensions/git.js
+++ native/v8_extensions/git.js
@@ -3,4 +3,11 @@ var $git = {};
 
   native function isRepository(path);
   $git.isRepository = isRepository;
+
+  native function getRepository(path);
+  $git.getRepository = getRepository;
+
+  native function getCurrentBranch(repository);
+  $git.getCurrentBranch = getCurrentBranch;
+
 })();
