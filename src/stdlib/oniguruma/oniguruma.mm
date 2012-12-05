547f3937f3ce2926881fdc57126a263e6abf8db6
Corey Johnson & Nathan Sobo <cj+nathan@github.com>
2012-12-04 16:50:30 -0800

    Move OnigScannerUserData into OnigScanner

--- src/stdlib/oniguruma/oniguruma.mm
+++ src/stdlib/oniguruma/oniguruma.mm
@@ -8,121 +8,21 @@
 
 using namespace v8;
 
-class OnigScannerUserData  {
-  public:
-  OnigScannerUserData(Handle<Array> sources) {
-    int length = sources->Length();
-    regExps.resize(length);
-    cachedResults.resize(length);
+class OnigScanner : public node::ObjectWrap {
+ public:
+  static void Init(Handle<Object> target);
 
-    for (int i = 0; i < length; i++) {
-      String::Utf8Value utf8Value(sources->Get(i));
-      NSString *sourceString = [NSString stringWithUTF8String:*utf8Value];
-      regExps[i] = [[OnigRegexp compile:sourceString] retain];
-    }
-  }
+ private:
+  OnigScanner(Handle<Array> sources);
+  ~OnigScanner();
 
-  ~OnigScannerUserData() {
-    NSLog(@"OnigScannerUserData is being deconstructed");
-    for (std::vector<OnigRegexp *>::iterator iter = regExps.begin(); iter < regExps.end(); iter++) {
-      [*iter release];
-    }
-    for (std::vector<OnigResult *>::iterator iter = cachedResults.begin(); iter < cachedResults.end(); iter++) {
-      [*iter release];
-    }
-  }
+  static Handle<Value> New(const Arguments& args);
+  static Handle<Value> FindNextMatch(const Arguments& args);
 
-  Handle<Value> FindNextMatch(Handle<String> v8String, Handle<Number> v8StartLocation) {
-    String::Utf8Value utf8Value(v8String);
-    std::string string(*utf8Value);
-    int startLocation = v8StartLocation->Value();
-    int bestIndex = -1;
-    int bestLocation = NULL;
-    OnigResult *bestResult = NULL;
+  Handle<Value> FindNextMatch(Handle<String> v8String, Handle<Number> v8StartLocation);
+  Handle<Value> CaptureIndicesForMatch(OnigResult *result);
+  void ClearCachedResults();
 
-    bool useCachedResults = (string == lastMatchedString && startLocation >= lastStartLocation);
-    lastStartLocation = startLocation;
-
-    if (!useCachedResults) {
-      ClearCachedResults();
-      lastMatchedString = string;
-    }
-
-    std::vector<OnigRegexp *>::iterator iter = regExps.begin();
-    int index = 0;
-    while (iter < regExps.end()) {
-      OnigRegexp *regExp = *iter;
-
-      bool useCachedResult = false;
-      OnigResult *result = NULL;
-
-      // In Oniguruma, \G is based on the start position of the match, so the result
-      // changes based on the start position. So it can't be cached.
-      BOOL containsBackslashG = [regExp.expression rangeOfString:@"\\G"].location != NSNotFound;
-      if (useCachedResults && index <= maxCachedIndex && ! containsBackslashG) {
-        result = cachedResults[index];
-        useCachedResult = (result == NULL || [result locationAt:0] >= startLocation);
-      }
-
-      if (!useCachedResult) {
-        result = [regExp search:[NSString stringWithUTF8String:string.c_str()] start:startLocation];
-        cachedResults[index] = [result retain];
-        maxCachedIndex = index;
-      }
-
-      if ([result count] > 0) {
-        int location = [result locationAt:0];
-        if (bestIndex == -1 || location < bestLocation) {
-          bestLocation = location;
-          bestResult = result;
-          bestIndex = index;
-        }
-
-        if (location == startLocation) {
-          break;
-        }
-      }
-
-      iter++;
-      index++;
-    }
-
-    if (bestIndex >= 0) {
-      Local<Object> result = Object::New();
-      result->Set(String::NewSymbol("index"), Number::New(bestIndex));
-      result->Set(String::NewSymbol("captureIndices"), CaptureIndicesForMatch(bestResult));
-      return result;
-    }
-    else {
-      return Null();
-    }
-  }
-
-  void ClearCachedResults() {
-    maxCachedIndex = -1;
-    for (std::vector<OnigResult *>::iterator iter = cachedResults.begin(); iter < cachedResults.end(); iter++) {
-      [*iter release];
-      *iter = NULL;
-    }
-  }
-
-  Handle<Value> CaptureIndicesForMatch(OnigResult *result) {
-    Local<Array> array = Array::New([result count] * 3);
-    int i = 0;
-    int resultCount = [result count];
-    for (int index = 0; index < resultCount; index++) {
-      int captureLength = [result lengthAt:index];
-      int captureStart = [result locationAt:index];
-
-      array->Set(i++, Number::New(index));
-      array->Set(i++, Number::New(captureStart));
-      array->Set(i++, Number::New(captureStart + captureLength));
-    }
-
-    return array;
-  }
-
-  protected:
   std::vector<OnigRegexp *> regExps;
   std::string lastMatchedString;
   std::vector<OnigResult *> cachedResults;
@@ -130,27 +30,25 @@ class OnigScannerUserData  {
   int lastStartLocation;
 };
 
-class OnigScanner : public node::ObjectWrap {
- public:
-  static void Init(Handle<Object> target);
+OnigScanner::OnigScanner(Handle<Array> sources) {
+  int length = sources->Length();
+  regExps.resize(length);
+  cachedResults.resize(length);
 
- private:
-  OnigScanner(OnigScannerUserData *userData);
-  ~OnigScanner();
-
-  static Handle<Value> New(const Arguments& args);
-  static Handle<Value> FindNextMatch(const   Arguments& args);
-
-  OnigScannerUserData *userData_;
-};
-
-OnigScanner::OnigScanner(OnigScannerUserData *userData) {
-  userData_ = userData;
+  for (int i = 0; i < length; i++) {
+    String::Utf8Value utf8Value(sources->Get(i));
+    NSString *sourceString = [NSString stringWithUTF8String:*utf8Value];
+    regExps[i] = [[OnigRegexp compile:sourceString] retain];
+  }
 };
 
 OnigScanner::~OnigScanner() {
-  delete userData_;
-  NSLog(@"If you see this, it means OnigScanner is being deconstructed\n");
+  for (std::vector<OnigRegexp *>::iterator iter = regExps.begin(); iter < regExps.end(); iter++) {
+    [*iter release];
+  }
+  for (std::vector<OnigResult *>::iterator iter = cachedResults.begin(); iter < cachedResults.end(); iter++) {
+    [*iter release];
+  }
 };
 
 void OnigScanner::Init(Handle<Object> target) {
@@ -166,8 +64,7 @@ void OnigScanner::Init(Handle<Object> target) {
 
 Handle<Value> OnigScanner::New(const Arguments& args) {
   HandleScope scope;
-  OnigScannerUserData *onigUserData = new OnigScannerUserData(Local<Array>::Cast(args[0]));
-  OnigScanner* scanner = new OnigScanner(onigUserData);
+  OnigScanner* scanner = new OnigScanner(Local<Array>::Cast(args[0]));
   scanner->Wrap(args.This());
   return args.This();
 }
@@ -175,11 +72,101 @@ Handle<Value> OnigScanner::New(const Arguments& args) {
 Handle<Value> OnigScanner::FindNextMatch(const Arguments& args) {
   HandleScope scope;
   OnigScanner* scanner = node::ObjectWrap::Unwrap<OnigScanner>(args.This());
-  return scope.Close(scanner->userData_->FindNextMatch(Local<String>::Cast(args[0]), Local<Number>::Cast(args[1])));
+  return scope.Close(scanner->FindNextMatch(Local<String>::Cast(args[0]), Local<Number>::Cast(args[1])));
 }
 
 void Init(Handle<Object> target) {
   OnigScanner::Init(target);
+}
+
+Handle<Value> OnigScanner::FindNextMatch(Handle<String> v8String, Handle<Number> v8StartLocation) {
+  String::Utf8Value utf8Value(v8String);
+  std::string string(*utf8Value);
+  int startLocation = v8StartLocation->Value();
+  int bestIndex = -1;
+  int bestLocation = NULL;
+  OnigResult *bestResult = NULL;
+
+  bool useCachedResults = (string == lastMatchedString && startLocation >= lastStartLocation);
+  lastStartLocation = startLocation;
+
+  if (!useCachedResults) {
+    ClearCachedResults();
+    lastMatchedString = string;
+  }
+
+  std::vector<OnigRegexp *>::iterator iter = regExps.begin();
+  int index = 0;
+  while (iter < regExps.end()) {
+    OnigRegexp *regExp = *iter;
+
+    bool useCachedResult = false;
+    OnigResult *result = NULL;
+
+    // In Oniguruma, \G is based on the start position of the match, so the result
+    // changes based on the start position. So it can't be cached.
+    BOOL containsBackslashG = [regExp.expression rangeOfString:@"\\G"].location != NSNotFound;
+    if (useCachedResults && index <= maxCachedIndex && ! containsBackslashG) {
+      result = cachedResults[index];
+      useCachedResult = (result == NULL || [result locationAt:0] >= startLocation);
+    }
+
+    if (!useCachedResult) {
+      result = [regExp search:[NSString stringWithUTF8String:string.c_str()] start:startLocation];
+      cachedResults[index] = [result retain];
+      maxCachedIndex = index;
+    }
+
+    if ([result count] > 0) {
+      int location = [result locationAt:0];
+      if (bestIndex == -1 || location < bestLocation) {
+        bestLocation = location;
+        bestResult = result;
+        bestIndex = index;
+      }
+
+      if (location == startLocation) {
+        break;
+      }
+    }
+
+    iter++;
+    index++;
+  }
+
+  if (bestIndex >= 0) {
+    Local<Object> result = Object::New();
+    result->Set(String::NewSymbol("index"), Number::New(bestIndex));
+    result->Set(String::NewSymbol("captureIndices"), CaptureIndicesForMatch(bestResult));
+    return result;
+  }
+  else {
+    return Null();
+  }
+}
+
+void OnigScanner::ClearCachedResults() {
+  maxCachedIndex = -1;
+  for (std::vector<OnigResult *>::iterator iter = cachedResults.begin(); iter < cachedResults.end(); iter++) {
+    [*iter release];
+    *iter = NULL;
+  }
+}
+
+Handle<Value> OnigScanner::CaptureIndicesForMatch(OnigResult *result) {
+  Local<Array> array = Array::New([result count] * 3);
+  int i = 0;
+  int resultCount = [result count];
+  for (int index = 0; index < resultCount; index++) {
+    int captureLength = [result lengthAt:index];
+    int captureStart = [result locationAt:index];
+
+    array->Set(i++, Number::New(index));
+    array->Set(i++, Number::New(captureStart));
+    array->Set(i++, Number::New(captureStart + captureLength));
+  }
+
+  return array;
 }
 
 NODE_MODULE(oniguruma, Init)
