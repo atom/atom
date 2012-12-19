75b9e7cbb1e2edab37295f8139709f0f26168ede
Kevin Sawicki <kevin@github.com>
2012-12-19 15:29:22 -0800

    Replace & with %26 when encoding URL params
    This is not handled by stringByAddingPercentEscapesUsingEncoding

--- native/atom_window_controller.mm
+++ native/atom_window_controller.mm
@@ -92,6 +92,7 @@
 - (NSString *)encodeUrlParam:(NSString *)param {
   param = [param stringByReplacingOccurrencesOfString:@" " withString:@"%20"];
   param = [param stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+  param = [param stringByReplacingOccurrencesOfString:@"&" withString:@"%26"];
   return param;
 }
 
