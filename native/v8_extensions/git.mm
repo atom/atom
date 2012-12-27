219aaca0f52f8bb34bb502823f06b2c951b3c670
Kevin Sawicki <kevin@github.com>
2012-12-27 10:41:02 -0800

    Show diff stat in status bar
    Include the number of lines added and removed for
    new and modified files

--- native/v8_extensions/git.mm
+++ native/v8_extensions/git.mm
@@ -105,6 +105,80 @@ public:
     }
   }
 
+  CefRefPtr<CefV8Value> GetDiffStats(const char *path) {
+    if (!exists) {
+      return CefV8Value::CreateNull();
+    }
+
+    char *copiedPath = (char *)malloc(sizeof(char) * (strlen(path) + 1));
+    strcpy(copiedPath, path);
+    git_diff_options options;
+    memset(&options, 0, sizeof(options));
+    git_strarray paths;
+    paths.count = 1;
+    paths.strings = &copiedPath;
+    options.pathspec = paths;
+    options.context_lines = 1;
+    options.flags = GIT_DIFF_DISABLE_PATHSPEC_MATCH;
+
+    git_reference *head;
+    if (git_repository_head(&head, repo) != GIT_OK) {
+      return CefV8Value::CreateNull();
+    }
+
+    const git_oid* sha = git_reference_oid(head);
+    git_commit *commit;
+    int commitStatus = git_commit_lookup(&commit, repo, sha);
+    git_reference_free(head);
+    if (commitStatus != GIT_OK) {
+      return CefV8Value::CreateNull();
+    }
+
+    git_tree *tree;
+    int treeStatus = git_commit_tree(&tree, commit);
+    git_commit_free(commit);
+    if (treeStatus != GIT_OK) {
+      return CefV8Value::CreateNull();
+    }
+
+    git_diff_list *diffs;
+    int diffStatus = git_diff_workdir_to_tree(repo, &options, tree, &diffs);
+    if (diffStatus != GIT_OK) {
+      return CefV8Value::CreateNull();
+    }
+
+    git_diff_patch *patch;
+    int patchStatus = git_diff_get_patch(&patch, NULL, diffs, 0);
+    git_diff_list_free(diffs);
+    if (patchStatus != GIT_OK) {
+      return CefV8Value::CreateNull();
+    }
+
+    int added = 0;
+    int deleted = 0;
+    int hunks = git_diff_patch_num_hunks(patch);
+    for (int i = 0; i < hunks; i++) {
+      int lines = git_diff_patch_num_lines_in_hunk(patch, i);
+      for (int j = 0; j < lines; j++) {
+        char lineType[2];
+        lineType[1] = '\0';
+        if (git_diff_patch_get_line_in_hunk(lineType, NULL, NULL, NULL, NULL, patch, i, j) == GIT_OK) {
+          if (strcmp(lineType, "+") == 0) {
+            added++;
+          } else if(strcmp(lineType, "-") == 0) {
+            deleted++;
+          }
+        }
+      }
+    }
+    git_diff_patch_free(patch);
+
+    CefRefPtr<CefV8Value> result = CefV8Value::CreateObject(NULL);
+    result->SetValue("added", CefV8Value::CreateInt(added), V8_PROPERTY_ATTRIBUTE_NONE);
+    result->SetValue("deleted", CefV8Value::CreateInt(deleted), V8_PROPERTY_ATTRIBUTE_NONE);
+    return result;
+  }
+
   IMPLEMENT_REFCOUNTING(GitRepository);
 };
 
@@ -153,6 +227,12 @@ bool Git::Execute(const CefString& name,
   if (name == "checkoutHead") {
     GitRepository *userData = (GitRepository *)object->GetUserData().get();
     retval = userData->CheckoutHead(arguments[0]->GetStringValue().ToString().c_str());
+    return true;
+  }
+
+  if (name == "getDiffStats") {
+    GitRepository *userData = (GitRepository *)object->GetUserData().get();
+    retval = userData->GetDiffStats(arguments[0]->GetStringValue().ToString().c_str());
     return true;
   }
 
