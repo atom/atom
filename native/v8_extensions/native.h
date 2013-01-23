8032503f11dc069690056925fac064fe844c9c02
Corey Johnson & Nathan Sobo <cj+nathan@github.com>
2013-01-23 12:14:22 -0700

    :lipstick:

--- native/v8_extensions/native.h
+++ native/v8_extensions/native.h
@@ -5,24 +5,22 @@
 namespace v8_extensions {
 
 class Native : public CefV8Handler {
-public:
+  public:
+    static void CreateContextBinding(CefRefPtr<CefV8Context> context);
+    virtual bool Execute(const CefString& name,
+                                        CefRefPtr<CefV8Value> object,
+                                        const CefV8ValueList& arguments,
+                                        CefRefPtr<CefV8Value>& retval,
+                                        CefString& exception) OVERRIDE;
 
-  static void CreateContextBinding(CefRefPtr<CefV8Context> context);
-  virtual bool Execute(const CefString& name,
-                                      CefRefPtr<CefV8Value> object,
-                                      const CefV8ValueList& arguments,
-                                      CefRefPtr<CefV8Value>& retval,
-                                      CefString& exception) OVERRIDE;
+    // Provide the reference counting implementation for this class.
+    IMPLEMENT_REFCOUNTING(Native);
 
-  // Provide the reference counting implementation for this class.
-  IMPLEMENT_REFCOUNTING(Native);
-
-private:
-  Native();
-  Native(Native const&);
-  static CefRefPtr<CefV8Handler> GetInstance();
-  void operator=(Native const&);
-  std::string windowState;
-};
-
+  private:
+    static CefRefPtr<CefV8Handler> GetInstance();
+    Native();
+    Native(Native const&);
+    void operator=(Native const&);
+    std::string windowState;
+  };
 }
