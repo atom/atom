atom/atom

commit 9cc4c2e5de623c323545074a85d49fbc7fee90d4

Allow `off` to be called before `on` without error
Previously calling `something.off 'event-name', handler` would throw
an error unless `on` was called first.

src/app/event-emitter.coffee
@@ -78,8 +78,8 @@ module.exports =
                 @off eventName, handler
         else
           subscriptionCountBefore = @subscriptionCount()
-          if handler
-            _.remove(@eventHandlersByEventName[eventName], handler)
+          if eventHandlers = @eventHandlersByEventName[eventName]
+            _.remove(eventHandlers, handler)
           else
             delete @eventHandlersByEventName?[eventName]
           @afterUnsubscribe?() if @subscriptionCount() < subscriptionCountBefore

spec/app/event-emitter-spec.coffee
@@ -109,6 +109,9 @@ describe "EventEmitter mixin", ->
         expect(fooHandler1).not.toHaveBeenCalled()
         expect(fooHandler2).toHaveBeenCalled()
 
+      it "does not throw an exception if there was not matching `on` call", ->
+        expect(-> object.off 'marco', -> "nothing").not.toThrow()
+
     describe "when there are namespaced event handlers", ->
       [barHandler2, bazHandler1, bazHandler2, bazHandler3] = []
 

commit cd7162b3bc20b9279d06601953fb5dd32328b234

Fix event emitter error
Introduced in 9cc4c2e5de623c323545074a85d49fbc7fee90d4 because
of botched merge conflict resolution

src/app/event-emitter.coffee
@@ -78,8 +78,9 @@ module.exports =
                 @off eventName, handler
         else
           subscriptionCountBefore = @subscriptionCount()
-          if eventHandlers = @eventHandlersByEventName[eventName]
-            _.remove(eventHandlers, handler)
+          if handler
+            eventHandlers = @eventHandlersByEventName[eventName]
+            _.remove(eventHandlers, handler) if eventHandlers
           else
             delete @eventHandlersByEventName?[eventName]
           @afterUnsubscribe?() if @subscriptionCount() < subscriptionCountBefore