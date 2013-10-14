1d343dd483739da940165bc8d43888338f62786c
Cheng Zhao <zcbenz@gmail.com>
2013-10-14 15:09:17 +0800

    Make the safeSpawn function a util module.

---
+++ script/utils/child-process-wrapper.js
@@ -0,0 +1,35 @@
+var childProcess = require('child_process');
+
+// Exit the process if the command failed and only call the callback if the
+// command succeed, output of the command would also be piped.
+exports.safeExec = function(command, options, callback) {
+  if (!callback) {
+    callback = options;
+    options = {};
+  }
+  var child = childProcess.exec(command, options, function(error, stdout, stderr) {
+    if (error)
+      process.exit(error.code);
+    else
+      callback(0);
+  });
+  child.stderr.pipe(process.stderr);
+  child.stdout.pipe(process.stdout);
+}
+
+// Same with safeExec but call child_process.spawn instead.
+exports.safeSpawn = function(command, args, options, callback) {
+  if (!callback) {
+    callback = options;
+    options = {};
+  }
+  var child = childProcess.spawn(command, args, options);
+  child.stderr.pipe(process.stderr);
+  child.stdout.pipe(process.stdout);
+  child.on('exit', function(code) {
+    if (code != 0)
+      process.exit(code);
+    else
+      callback(code);
+  });
+}
