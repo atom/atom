b7e07fdbcbe8bd71291243bef80e02aebea0ccb0
Paul Betts <paul@paulbetts.org>
2013-10-14 12:13:39 -0700

    Bail if Node isn't installed

--- script/bootstrap.ps1
+++ script/bootstrap.ps1
@@ -1,6 +1,11 @@
 git submodule --quiet sync
 git submodule --quiet update --recursive --init
 
+if (!(Get-Command "npm")) {
+    Write-Error "You need to install Node and have npm on the PATH"
+    Break
+}
+
 cd vendor/apm
 npm install --silent .
 
