3e8e97b02e3686568be31c90012c9616b0fd2c51
Kevin Sawicki <kevin@github.com>
2014-07-10 14:13:40 -0700

    Download 32-bit node on Windows CI

--- script/download-node.js
+++ script/download-node.js
@@ -58,7 +58,10 @@ var copyNodeBinToLocation = function(callback, version, targetFilename, fromDire
 var downloadNode = function(version, done) {
   var arch, downloadURL, filename;
   if (process.platform === 'win32') {
-    arch = process.arch === 'x64' ? 'x64/' : '';
+    if (process.env.JANKY_SHA1)
+      arch = ''
+    else
+      arch = process.arch === 'x64' ? 'x64/' : '';
     downloadURL = "http://nodejs.org/dist/" + version + "/" + arch + "node.exe";
     filename = path.join('bin', "node.exe");
   } else {
