42ab14c2cb59c46bcac0ed8dcd1a185dde17764b
Kevin Sawicki <kevin@github.com>
2014-12-11 10:50:06 -0800

    Add initial atom.exe wrapper

---
+++ resources/win/atom.js
@@ -0,0 +1,7 @@
+var spawn = require('child_process').spawn;
+
+var options = {
+  detached: true,
+  stdio: 'ignore'
+}
+spawn("C:\\Users\\kevin\\AppData\\Local\\atom\\app-0.156.0\\atom.exe", [], options);
