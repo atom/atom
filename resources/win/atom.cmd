3dced04aabd84c5930c96d5ea1baa72537110af8
Kevin Sawicki <kevin@github.com>
2014-12-11 12:49:09 -0800

    Add -t and --test to expected output args

--- resources/win/atom.cmd
+++ resources/win/atom.cmd
@@ -3,12 +3,14 @@
 SET EXPECT_OUTPUT=
 
 FOR %%a IN (%*) DO (
-  IF /I "%%a"=="-h"           SET EXPECT_OUTPUT=YES
-  IF /I "%%a"=="--help"       SET EXPECT_OUTPUT=YES
-  IF /I "%%a"=="-v"           SET EXPECT_OUTPUT=YES
-  IF /I "%%a"=="--version"    SET EXPECT_OUTPUT=YES
   IF /I "%%a"=="-f"           SET EXPECT_OUTPUT=YES
   IF /I "%%a"=="--foreground" SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="-h"           SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="--help"       SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="-t"           SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="--test"       SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="-v"           SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="--version"    SET EXPECT_OUTPUT=YES
   IF /I "%%a"=="-w"           SET EXPECT_OUTPUT=YES
   IF /I "%%a"=="--wait"       SET EXPECT_OUTPUT=YES
 )
