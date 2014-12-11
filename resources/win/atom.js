330592e2bdd8cae278b2ac428481e3523f612189
Kevin Sawicki <kevin@github.com>
2014-12-11 10:56:36 -0800

    Add atom.cmd

--- resources/win/atom.js
+++ resources/win/atom.js
@@ -4,5 +4,7 @@ var options = {
   detached: true,
   stdio: 'ignore'
 }
+
+console.log(process.argv);
 spawn("C:\\Users\\kevin\\AppData\\Local\\atom\\app-0.156.0\\atom.exe", [], options);
 process.exit();
