24365b95df18b79fd8f995007a5d2735fecae424
Antonio Scandurra <me@as-cii.com>
2015-10-08 14:07:28 +0200

    :lipstick: Satisfy linter requirements

--- src/timeout-cop.js
+++ src/timeout-cop.js
@@ -1,12 +1,14 @@
-var parentProcessId = process.argv[2];
-var timeoutInMinutes = process.argv[3];
-var timeoutInMilliseconds = timeoutInMinutes * 1000 * 60
+'use strict'
 
-function exitTestRunner() {
-  process.kill(parentProcessId, "SIGINT");
-  var errorMessage = "The test suite has timed out because it has been running";
-  errorMessage += " for more than " + timeoutInMinutes + " minutes.";
-  console.log(errorMessage);
+let parentProcessId = process.argv[2]
+let timeoutInMinutes = process.argv[3]
+let timeoutInMilliseconds = timeoutInMinutes * 1000 * 60
+
+function exitTestRunner () {
+  process.kill(parentProcessId, 'SIGINT')
+  let errorMessage = 'The test suite has timed out because it has been running'
+  errorMessage += ' for more than ' + timeoutInMinutes + ' minutes.'
+  console.log(errorMessage)
 }
 
-setTimeout(exitTestRunner, timeoutInMilliseconds);
+setTimeout(exitTestRunner, timeoutInMilliseconds)
