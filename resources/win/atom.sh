ae09d687a0c194b6adefb8eb9450c8d5689716d0
Thomas Johansen <thomasjo@gmail.com>
2015-10-23 22:02:33 +0200

    Re-enable console.log via ELECTRON_ENABLE_LOGGING

--- resources/win/atom.sh
+++ resources/win/atom.sh
@@ -18,6 +18,7 @@ done
 directory=$(dirname "$0")
 
 if [ $EXPECT_OUTPUT ]; then
+  export ELECTRON_ENABLE_LOGGING=1
   "$directory/../../atom.exe" "$@"
 else
   "$directory/../app/apm/bin/node.exe" "$directory/atom.js" "$@"
