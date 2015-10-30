41d3d7a0c037a72afd22eff2980d03d6c9d20211
Daniel Hengeveld <danielwh@gmail.com>
2015-10-30 19:11:28 +0100

    Start adding the troublesome buffer-events first

--- spec/git-repository-async-spec.js
+++ spec/git-repository-async-spec.js
@@ -1,5 +1,6 @@
 'use babel'
 
+const fs = require('fs-plus')
 const path = require('path')
 const temp = require('temp')
 
@@ -9,8 +10,15 @@ const openFixture = (fixture) => {
   GitRepositoryAsync.open(path.join(__dirname, 'fixtures', 'git', fixture))
 }
 
+const copyRepository = () => {
+  let workingDirPath = temp.mkdirSync('atom-working-dir')
+  fs.copySync(path.join(__dirname, 'fixtures', 'git', 'working-dir'), workingDirPath)
+  fs.renameSync(path.join(workingDirPath, 'git.git'), path.join(workingDirPath, '.git'))
+  return fs.realpathSync(workingDirPath)
+}
 
-fdescribe('GitRepositoryAsync', () => {
+
+describe('GitRepositoryAsync', () => {
   describe('@open(path)', () => {
     it('repo is null when no repository is found', () => {
       let repo = GitRepositoryAsync.open(path.join(temp.dir, 'nogit.txt'))
@@ -26,6 +34,8 @@ fdescribe('GitRepositoryAsync', () => {
   })
 
   describe('.getPath()', () => {
+    xit('returns the repository path for a .git directory path')
+
     it('returns the repository path for a repository path', () => {
       let repo = openFixture('master.git')
       let onSuccess = jasmine.createSpy('onSuccess')
@@ -38,4 +48,38 @@ fdescribe('GitRepositoryAsync', () => {
       })
     })
   })
+
+  fdescribe('buffer events', () => {
+    beforeEach(() => {
+      // This is sync, should be fine in a beforeEach
+      atom.project.setPaths([copyRepository()])
+    })
+
+    it('emits a status-changed events when a buffer is saved', () => {
+      let editor, called
+      waitsForPromise(function () {
+        return atom.workspace.open('other.txt').then((o) => {
+          editor = o
+        })
+      })
+
+      runs(() => {
+        editor.insertNewline()
+        let repo = atom.project.getRepositories()[0]
+        repo.async.onDidChangeStatus((c) => {
+          called = c
+        })
+        editor.save()
+      })
+
+      waitsFor(() => {
+        return Boolean(called)
+      })
+
+      runs(() => {
+        expect(called).toEqual({path: editor.getPath(), pathStatus: 256})
+      })
+    })
+  })
+
 })
