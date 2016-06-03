e7faf616b9e9118bddfd46e873ea789752e06f01
Michelle Tilley <binarymuse@github.com>
2016-06-03 11:38:33 -0700

    Fix version check output for Windows

--- script/check-version.js
+++ script/check-version.js
@@ -19,7 +19,11 @@ getBundledNodeVersion(bundledNodePath, function(err, bundledVersion) {
 
   if (ourVersion !== bundledVersion) {
     console.error('System node (' + ourVersion + ') does not match bundled node (' + bundledVersion + ').');
-    console.error('Please use `./bin/node` to run node, and use `./bin/npm` to run npm scripts.')
+    if (process.platform !== 'win32') {
+      console.error('Please use `.\\bin\\node.exe` to run node, and use `.\\bin\\npm.cmd` to run npm scripts.')
+    } else {
+      console.error('Please use `./bin/node` to run node, and use `./bin/npm` to run npm scripts.')
+    }
     process.exit(1)
   } else {
     process.exit(0)
