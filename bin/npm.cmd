c27a57e6bc4fe2cbe5c2cd7a9602a55f2f592bc0
Michelle Tilley <binarymuse@github.com>
2016-06-03 14:41:02 -0700

    Don't set custom node-gyp path until after deduping

--- bin/npm.cmd
+++ bin/npm.cmd
@@ -2,5 +2,8 @@
 setlocal
 
 set PATH=%~dp0;%PATH%
-set npm_config_node_gyp=%~dp0;\..\node_modules\.bin\node-gyp
+set maybe_node_gyp_path=%~dp0;\..\node_modules\.bin\node-gyp
+if exist %maybe_node_gyp_path% (
+  set npm_config_node_gyp=%maybe_node_gyp_path%
+)
 .\node_modules\.bin\npm.cmd %*
