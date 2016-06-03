5105d74cf9ac495557d553cc8363197b70fb358e
Michelle Tilley <binarymuse@github.com>
2016-06-03 11:39:03 -0700

    Fix bad check

--- script/check-version.js
+++ script/check-version.js
@@ -19,7 +19,7 @@ getBundledNodeVersion(bundledNodePath, function(err, bundledVersion) {
 
   if (ourVersion !== bundledVersion) {
     console.error('System node (' + ourVersion + ') does not match bundled node (' + bundledVersion + ').');
-    if (process.platform !== 'win32') {
+    if (process.platform === 'win32') {
       console.error('Please use `.\\bin\\node.exe` to run node, and use `.\\bin\\npm.cmd` to run npm scripts.')
     } else {
       console.error('Please use `./bin/node` to run node, and use `./bin/npm` to run npm scripts.')
