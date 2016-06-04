141b09e7cf218d7b98afc715d152d899a4154363
Michelle Tilley <binarymuse@github.com>
2016-06-03 19:19:40 -0700

    Set node-gyp path after deduping
    Signed-off-by: Katrina Uychaco <kuychaco@github.com>

--- bin/apm.cmd
+++ bin/apm.cmd
@@ -1,3 +1,8 @@
+set maybe_node_gyp_path=%~dp0\..\node_modules\.bin\node-gyp
+if exist %maybe_node_gyp_path% (
+  set npm_config_node_gyp=%maybe_node_gyp_path%
+)
+
 @IF EXIST "%~dp0\node.exe" (
   "%~dp0\node.exe" "%~dp0/../lib/cli.js" %*
 ) ELSE (
