265cb90a6daf9df116166a62346276ef0f19a93d
Antonio Scandurra <as-cii@github.com>
2016-07-26 14:51:51 +0200

    Clean output directory before building

--- build/build.js
+++ build/build.js
@@ -2,8 +2,10 @@
 
 'use strict'
 
+const cleanOutputDirectory = require('./lib/clean-output-directory')
 const transpileBabelPaths = require('./lib/transpile-babel-paths')
 const transpileCoffeeScriptPaths = require('./lib/transpile-coffee-script-paths')
 
+cleanOutputDirectory()
 transpileBabelPaths()
 transpileCoffeeScriptPaths()
---
+++ build/lib/clean-output-directory.js
@@ -0,0 +1,10 @@
+const fs = require('fs-extra')
+const path = require('path')
+const CONFIG = require('../config')
+
+module.exports = function () {
+  if (fs.existsSync(CONFIG.buildOutputPath)) {
+    console.log(`Cleaning ${CONFIG.buildOutputPath}...`);
+    fs.removeSync(CONFIG.buildOutputPath)
+  }
+}
