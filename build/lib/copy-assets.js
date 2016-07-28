18c20c095f7595955c2a45235fd1528808991aa2
Antonio Scandurra <as-cii@github.com>
2016-07-28 16:07:40 +0200

    Don't copy paths that will be ignored

--- build/lib/copy-assets.js
+++ build/lib/copy-assets.js
@@ -22,12 +22,81 @@ module.exports = function () {
     path.join(CONFIG.repositoryRootPath, 'vendor')
   ]
   srcPaths = srcPaths.concat(glob.sync(path.join(CONFIG.repositoryRootPath, 'spec', '*.*'), {ignore: path.join('**', '*-spec.*')}))
+  const ignoredPathsRegExp = buildIgnoredPathsRegExp()
   for (let srcPath of srcPaths) {
-    fs.copySync(srcPath, computeDestinationPath(srcPath))
+    fs.copySync(
+      srcPath,
+      computeDestinationPath(srcPath),
+      {filter: (path) => !ignoredPathsRegExp.test(path)}
+    )
   }
 }
 
 function computeDestinationPath (srcPath) {
   const relativePath = path.relative(CONFIG.repositoryRootPath, srcPath)
   return path.join(CONFIG.intermediateAppPath, relativePath)
+}
+
+function buildIgnoredPathsRegExp () {
+  const ignoreRegExps = [
+    escapeRegExp(path.join('git-utils', 'deps')),
+    escapeRegExp(path.join('oniguruma', 'deps')),
+    escapeRegExp(path.join('less', 'dist')),
+    escapeRegExp(path.join('pegjs', 'examples')),
+    escapeRegExp(path.join('get-parameter-names', 'node_modules', 'testla')),
+    escapeRegExp(path.join('get-parameter-names', 'node_modules', '.bin', 'testla')),
+    escapeRegExp(path.join('jasmine-reporters', 'ext')),
+    escapeRegExp(path.join('node_modules', 'nan')),
+    escapeRegExp(path.join('node_modules', 'native-mate')),
+    escapeRegExp(path.join('build', 'binding.Makefile')),
+    escapeRegExp(path.join('build', 'config.gypi')),
+    escapeRegExp(path.join('build', 'gyp-mac-tool')),
+    escapeRegExp(path.join('build', 'Makefile')),
+    escapeRegExp(path.join('build', 'Release', 'obj.target')),
+    escapeRegExp(path.join('build', 'Release', 'obj')),
+    escapeRegExp(path.join('build', 'Release', '.deps')),
+    escapeRegExp(path.join('vendor', 'apm')),
+
+    // These are only required in dev-mode, when pegjs grammars aren't precompiled
+    escapeRegExp(path.join('node_modules', 'loophole')),
+    escapeRegExp(path.join('node_modules', 'pegjs')),
+    escapeRegExp(path.join('node_modules', '.bin', 'pegjs')),
+    escapeRegExp(path.join('node_modules', 'spellchecker', 'vendor', 'hunspell') + path.sep) + '.*',
+    escapeRegExp(path.join('build', 'Release') + path.sep) + '.*\\.pdb',
+
+    // Ignore *.cc and *.h files from native modules
+    escapeRegExp(path.join('ctags', 'src') + path.sep) + '.*\\.(cc|h)*',
+    escapeRegExp(path.join('git-utils', 'src') + path.sep) + '.*\\.(cc|h)*',
+    escapeRegExp(path.join('keytar', 'src') + path.sep) + '.*\\.(cc|h)*',
+    escapeRegExp(path.join('nslog', 'src') + path.sep) + '.*\\.(cc|h)*',
+    escapeRegExp(path.join('oniguruma', 'src') + path.sep) + '.*\\.(cc|h)*',
+    escapeRegExp(path.join('pathwatcher', 'src') + path.sep) + '.*\\.(cc|h)*',
+    escapeRegExp(path.join('runas', 'src') + path.sep) + '.*\\.(cc|h)*',
+    escapeRegExp(path.join('scrollbar-style', 'src') + path.sep) + '.*\\.(cc|h)*',
+    escapeRegExp(path.join('spellchecker', 'src') + path.sep) + '.*\\.(cc|h)*',
+    escapeRegExp(path.join('cached-run-in-this-context', 'src') + path.sep) + '.*\\.(cc|h)?',
+    escapeRegExp(path.join('keyboard-layout', 'src') + path.sep) + '.*\\.(cc|h|mm)*',
+
+    // Ignore build files
+    escapeRegExp(path.sep) + 'binding\\.gyp$',
+    escapeRegExp(path.sep) + '.+\\.target.mk$',
+    escapeRegExp(path.sep) + 'linker\\.lock$',
+    escapeRegExp(path.join('build', 'Release') + path.sep) + '.+\\.node\\.dSYM',
+
+    // Ignore test, spec and example folders for packages
+    'node_modules' + escapeRegExp(path.sep) + '.*' + escapeRegExp(path.sep) + '_*te?sts?_*' + escapeRegExp(path.sep),
+    'node_modules' + escapeRegExp(path.sep) + '.*' + escapeRegExp(path.sep) + 'spec' + escapeRegExp(path.sep),
+    'node_modules' + escapeRegExp(path.sep) + '.*' + escapeRegExp(path.sep) + 'examples?' + escapeRegExp(path.sep),
+  ]
+  // Ignore Hunspell dictionaries only on macOS.
+  if (process.platform === 'darwin') {
+    ignoreRegExps.push(escapeRegExp(path.join('spellchecker', 'vendor', 'hunspell_dictionaries')))
+  }
+
+  const regExpSource = ignoreRegExps.map(path => `(${path})`).join('|')
+  return new RegExp(regExpSource)
+}
+
+function escapeRegExp (string) {
+  return string.replace(/[.?*+^$[\]\\(){}|-]/g, "\\$&")
 }
--- build/lib/package-application.js
+++ build/lib/package-application.js
@@ -21,7 +21,6 @@ module.exports = function () {
     'build-version': CONFIG.appMetadata.version,
     'download': {cache: CONFIG.cachePath},
     'dir': CONFIG.intermediateAppPath,
-    'ignore': buildIgnoredPathsRegExp(),
     'out': CONFIG.buildOutputPath,
     'overwrite': true,
     'platform': process.platform,
@@ -47,84 +46,4 @@ function buildAsarUnpackGlobExpression () {
   ]
 
   return `{${unpack.join(',')}}`
-}
-
-function buildIgnoredPathsRegExp () {
-  const ignoreRegExps = [
-    escapeRegExp('.DS_Store'),
-    escapeRegExp('.jshintrc'),
-    escapeRegExp('.npmignore'),
-    escapeRegExp('.pairs'),
-    escapeRegExp('.travis.yml'),
-    escapeRegExp('appveyor.yml'),
-    escapeRegExp('.idea'),
-    escapeRegExp('.editorconfig'),
-    escapeRegExp('.lint'),
-    escapeRegExp('.lintignore'),
-    escapeRegExp('.eslintrc'),
-    escapeRegExp('.jshintignore'),
-    escapeRegExp('coffeelint.json'),
-    escapeRegExp('.coffeelintignore'),
-    escapeRegExp('.gitattributes'),
-    escapeRegExp('.gitkeep'),
-    escapeRegExp(path.join('git-utils', 'deps')),
-    escapeRegExp(path.join('oniguruma', 'deps')),
-    escapeRegExp(path.join('less', 'dist')),
-    escapeRegExp(path.join('pegjs', 'examples')),
-    escapeRegExp(path.join('get-parameter-names', 'node_modules', 'testla')),
-    escapeRegExp(path.join('get-parameter-names', 'node_modules', '.bin', 'testla')),
-    escapeRegExp(path.join('jasmine-reporters', 'ext')),
-    escapeRegExp(path.join('node_modules', 'nan')),
-    escapeRegExp(path.join('node_modules', 'native-mate')),
-    escapeRegExp(path.join('build', 'binding.Makefile')),
-    escapeRegExp(path.join('build', 'config.gypi')),
-    escapeRegExp(path.join('build', 'gyp-mac-tool')),
-    escapeRegExp(path.join('build', 'Makefile')),
-    escapeRegExp(path.join('build', 'Release', 'obj.target')),
-    escapeRegExp(path.join('build', 'Release', 'obj')),
-    escapeRegExp(path.join('build', 'Release', '.deps')),
-    escapeRegExp(path.join('vendor', 'apm')),
-
-    // These are only required in dev-mode, when pegjs grammars aren't precompiled
-    escapeRegExp(path.join('node_modules', 'loophole')),
-    escapeRegExp(path.join('node_modules', 'pegjs')),
-    escapeRegExp(path.join('node_modules', '.bin', 'pegjs')),
-    escapeRegExp(path.join('node_modules', 'spellchecker', 'vendor', 'hunspell') + path.sep) + '.*',
-    escapeRegExp(path.join('build', 'Release') + path.sep) + '.*\\.pdb',
-
-    // Ignore *.cc and *.h files from native modules
-    escapeRegExp(path.join('ctags', 'src') + path.sep) + '.*\\.(cc|h)*',
-    escapeRegExp(path.join('git-utils', 'src') + path.sep) + '.*\\.(cc|h)*',
-    escapeRegExp(path.join('keytar', 'src') + path.sep) + '.*\\.(cc|h)*',
-    escapeRegExp(path.join('nslog', 'src') + path.sep) + '.*\\.(cc|h)*',
-    escapeRegExp(path.join('oniguruma', 'src') + path.sep) + '.*\\.(cc|h)*',
-    escapeRegExp(path.join('pathwatcher', 'src') + path.sep) + '.*\\.(cc|h)*',
-    escapeRegExp(path.join('runas', 'src') + path.sep) + '.*\\.(cc|h)*',
-    escapeRegExp(path.join('scrollbar-style', 'src') + path.sep) + '.*\\.(cc|h)*',
-    escapeRegExp(path.join('spellchecker', 'src') + path.sep) + '.*\\.(cc|h)*',
-    escapeRegExp(path.join('cached-run-in-this-context', 'src') + path.sep) + '.*\\.(cc|h)?',
-    escapeRegExp(path.join('keyboard-layout', 'src') + path.sep) + '.*\\.(cc|h|mm)*',
-
-    // Ignore build files
-    escapeRegExp(path.sep) + 'binding\\.gyp$',
-    escapeRegExp(path.sep) + '.+\\.target.mk$',
-    escapeRegExp(path.sep) + 'linker\\.lock$',
-    escapeRegExp(path.join('build', 'Release') + path.sep) + '.+\\.node\\.dSYM',
-
-    // Ignore test, spec and example folders for packages
-    'node_modules' + escapeRegExp(path.sep) + '.*' + escapeRegExp(path.sep) + '_*te?sts?_*' + escapeRegExp(path.sep),
-    'node_modules' + escapeRegExp(path.sep) + '.*' + escapeRegExp(path.sep) + 'spec' + escapeRegExp(path.sep),
-    'node_modules' + escapeRegExp(path.sep) + '.*' + escapeRegExp(path.sep) + 'examples?' + escapeRegExp(path.sep),
-  ]
-  // Ignore Hunspell dictionaries only on macOS.
-  if (process.platform === 'darwin') {
-    ignoreRegExps.push(escapeRegExp(path.join('spellchecker', 'vendor', 'hunspell_dictionaries')))
-  }
-
-  const regExpSource = ignoreRegExps.map(path => `(${path})`).join('|')
-  return new RegExp(regExpSource)
-}
-
-function escapeRegExp (string) {
-  return string.replace(/[.?*+^$[\]\\(){}|-]/g, "\\$&")
 }
