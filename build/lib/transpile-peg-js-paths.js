bda6447f327fb3187f4caa75d71565c3eda60d84
Antonio Scandurra <as-cii@github.com>
2016-07-28 18:22:47 +0200

    :art:

--- build/lib/transpile-peg-js-paths.js
+++ build/lib/transpile-peg-js-paths.js
@@ -8,7 +8,7 @@ const path = require('path')
 const CONFIG = require('../config')
 
 module.exports = function () {
-  console.log(`Transpiling PEG.js paths in ${CONFIG.intermediateAppPath}...`);
+  console.log(`Transpiling PEG.js paths in ${CONFIG.intermediateAppPath}...`)
   for (let path of getPathsToTranspile()) {
     transpilePegJsPath(path)
   }
