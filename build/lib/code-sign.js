947de55fa4481b0d611ff16331a989d3e3e1fcf0
Nathan Sobo <nathan@github.com>
2016-08-01 16:04:05 -0600

    :art: Clarify build environment variables related to code-signing

--- build/lib/code-sign.js
+++ build/lib/code-sign.js
@@ -7,17 +7,17 @@ module.exports = function (packagedAppPath) {
     return
   }
 
-  console.log(`Unlocking keychain ${process.env.MAC_CODE_SIGNING_KEYCHAIN}`)
+  console.log(`Unlocking keychain ${process.env.ATOM_MAC_CODE_SIGNING_KEYCHAIN}`)
   childProcess.spawnSync('security', [
     'unlock-keychain',
-    '-p', process.env.MAC_CODE_SIGNING_KEYCHAIN_PASSWORD,
-    process.env.MAC_CODE_SIGNING_KEYCHAIN
+    '-p', process.env.ATOM_MAC_CODE_SIGNING_KEYCHAIN_PASSWORD,
+    process.env.ATOM_MAC_CODE_SIGNING_KEYCHAIN
   ], {stdio: 'inherit'})
 
   console.log(`Code-signing application at ${packagedAppPath}`)
   childProcess.spawnSync('codesign', [
     '--deep', '--force', '--verbose',
-    '--keychain', process.env.MAC_CODE_SIGNING_KEYCHAIN,
+    '--keychain', process.env.ATOM_MAC_CODE_SIGNING_KEYCHAIN,
     '--sign', 'Developer ID Application: GitHub', packagedAppPath
   ], {stdio: 'inherit'})
 }
