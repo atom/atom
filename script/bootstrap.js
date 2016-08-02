0b6b632af315bf34aeb77277b55275aa41ff50da
Antonio Scandurra <as-cii@github.com>
2016-08-02 14:35:58 +0200

    Move fingerprinting into script/bootstrap.js

--- script/bootstrap.js
+++ script/bootstrap.js
@@ -4,12 +4,21 @@
 
 const path = require('path')
 
+const cleanDependencies = require('./lib/clean-dependencies')
+const dependenciesFingerprint = require('./lib/dependencies-fingerprint')
 const installApm = require('./lib/install-apm')
 const installAtomDependencies = require('./lib/install-atom-dependencies')
 const installScriptDependencies = require('./lib/install-script-dependencies')
 const verifyMachineRequirements = require('./lib/verify-machine-requirements')
 
 verifyMachineRequirements()
+
+if (dependenciesFingerprint.isOutdated()) {
+  cleanDependencies()
+}
+
 installScriptDependencies()
 installApm()
 installAtomDependencies()
+
+dependenciesFingerprint.write()
--- script/build.js
+++ script/build.js
@@ -18,7 +18,6 @@ const transpileBabelPaths = require('./lib/transpile-babel-paths')
 const transpileCoffeeScriptPaths = require('./lib/transpile-coffee-script-paths')
 const transpileCsonPaths = require('./lib/transpile-cson-paths')
 const transpilePegJsPaths = require('./lib/transpile-peg-js-paths')
-const writeFingerprint = require('./lib/fingerprint').writeFingerprint
 
 cleanOutputDirectory()
 copyAssets()
@@ -29,7 +28,6 @@ transpilePegJsPaths()
 generateModuleCache()
 prebuildLessCache()
 generateMetadata()
-writeFingerprint()
 dumpSymbols()
   .then(packageApplication)
   .then(packagedAppPath => {
