5ff7af84b650d2486cacd3bf6dfc1d728bd753d6
Antonio Scandurra <as-cii@github.com>
2016-08-02 14:46:03 +0200

    Set ATOM_RESOURCE_PATH in install-atom-dependencies.js

--- script/lib/install-atom-dependencies.js
+++ script/lib/install-atom-dependencies.js
@@ -7,6 +7,8 @@ const CONFIG = require('../config')
 
 module.exports = function () {
   const installEnv = Object.assign({}, process.env)
+  // Set resource path so that apm can load metadata related to Atom.
+  installEnv.ATOM_RESOURCE_PATH = CONFIG.repositoryRootPath
   // Set our target (Electron) version so that node-pre-gyp can download the
   // proper binaries.
   installEnv.npm_config_target = CONFIG.appMetadata.electronVersion;
