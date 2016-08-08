71a606ab3e96fc4baf2aae9b20e8e90ff0c7175c
Antonio Scandurra <as-cii@github.com>
2016-08-08 16:07:17 +0200

    Manually set title when running electron-winstaller

--- script/lib/create-windows-installer.js
+++ script/lib/create-windows-installer.js
@@ -17,7 +17,8 @@ module.exports = function (packagedAppPath, codeSign) {
     loadingGif: path.join(CONFIG.repositoryRootPath, 'resources', 'win', 'loading.gif'),
     outputDirectory: CONFIG.buildOutputPath,
     remoteReleases: `https://atom.io/api/updates?version=${CONFIG.appMetadata.version}`,
-    setupIcon: path.join(CONFIG.repositoryRootPath, 'resources', 'app-icons', CONFIG.channel, 'atom.ico')
+    setupIcon: path.join(CONFIG.repositoryRootPath, 'resources', 'app-icons', CONFIG.channel, 'atom.ico'),
+    title: CONFIG.channel === 'beta' ? 'Atom Beta' : 'Atom'
   }
 
   if (codeSign && process.env.WIN_P12KEY_URL) {
