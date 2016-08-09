f9a9deb9458935b4650159c49a9c26683a50fe6a
Nathan Sobo <nathan@github.com>
2016-08-09 14:43:38 -0600

    Stringify/parse temporary window state to avoid nulling out references
    If the object passed via IPC contains any keys of the same reference,
    all but one of these keys will be nulled out. Converting to/from a
    string avoids this problem.

---
+++ spec/application-delegate-spec.js
@@ -0,0 +1,19 @@
+/** @babel */
+
+import {it, fit, ffit, fffit, beforeEach, afterEach} from './async-spec-helpers'
+import ApplicationDelegate from '../src/application-delegate'
+
+describe('ApplicationDelegate', function () {
+  describe('set/getTemporaryWindowState', function () {
+    it('can serialize object trees containing redundant child object references', async function () {
+      const applicationDelegate = new ApplicationDelegate()
+      const childObject = {c: 1}
+      const sentObject = {a: childObject, b: childObject}
+
+      await applicationDelegate.setTemporaryWindowState(sentObject)
+      const receivedObject = await applicationDelegate.getTemporaryWindowState()
+
+      expect(receivedObject).toEqual(sentObject)
+    })
+  })
+})
