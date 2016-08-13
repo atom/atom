0f325edbad100b78240f51f529d1295954ec5206
Nathan Sobo <nathan@github.com>
2016-08-13 09:53:59 -0600

    More logging

--- spec/main-process/atom-application.test.js
+++ spec/main-process/atom-application.test.js
@@ -380,6 +380,7 @@ describe('AtomApplication', function () {
     await window.loadedPromise
     await conditionPromise(() => {
       console.log('polling condition, current focused window is ' + (window.atomApplication.lastFocusedWindow ? window.atomApplication.lastFocusedWindow.id : 'NULL') + ' and new window is ' + window.id);
+      console.log('BrowserWindow.getFocusedWindow()', electron.BrowserWindow.getFocusedWindow().id);
       return window.atomApplication.lastFocusedWindow === window
     })
     console.log('<<< focused window');
