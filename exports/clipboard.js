052cf0dbda99d56bd01e483b1299e177860b62fd
Max Brunsfeld <maxbrunsfeld@gmail.com>
2016-08-25 09:54:21 -0700

    Add shims for deprecated shell, clipboard requires

---
+++ exports/clipboard.js
@@ -0,0 +1,7 @@
+module.exports = require('electron').clipboard
+
+const Grim = require('grim')
+Grim.deprecate('Use `require("electron").clipboard` instead of `require("clipboard")`')
+
+// Ensure each package that requires this shim causes a deprecation warning
+delete require.cache[__filename]
