e4274e57ed62e7a524446606c7d09a923b53f92e
Antonio Scandurra <as-cii@github.com>
2016-10-07 16:10:06 +0200

    Include Atom version in deprecation message

--- src/style-manager.js
+++ src/style-manager.js
@@ -291,7 +291,8 @@ function transformDeprecatedShadowDOMSelectors (css, context) {
   })
   let deprecationMessage
   if (transformedSelectors.length > 0) {
-    deprecationMessage = 'The contents of `atom-text-editor` elements are no longer encapsulated within a shadow DOM boundary. '
+    deprecationMessage = 'Starting from Atom v1.13.0, the contents of `atom-text-editor` elements '
+    deprecationMessage += 'are no longer encapsulated within a shadow DOM boundary. '
     deprecationMessage += 'This means you should stop using \`:host\` and \`::shadow\` '
     deprecationMessage += 'pseudo-selectors, and prepend all your syntax selectors with \`syntax--\`. '
     deprecationMessage += 'To prevent breakage with existing style sheets, Atom will automatically '
