00a20698b69a1c5a562e3a7d0776bd2c91fa1714
Antonio Scandurra <as-cii@github.com>
2016-10-14 15:56:26 +0200

    Make large file construction benchmark easier

--- benchmarks/text-editor-large-file-construction.bench.js
+++ benchmarks/text-editor-large-file-construction.bench.js
@@ -5,24 +5,15 @@ import temp from 'temp'
 import {TextEditor, TextBuffer} from 'atom'
 
 export default function ({test}) {
-  const data = []
-  const maxLineCount = test ? 5 : 5000
-  const step = test ? 1 : 125
-  const lineText = 'Lorem ipsum dolor sit amet\n'
-  const sampleText = lineText.repeat(maxLineCount)
-  for (let lineCount = 0; lineCount <= maxLineCount; lineCount += step) {
-    const text = sampleText.slice(0, lineText.length * lineCount)
-    const buffer = new TextBuffer(text)
-    const t0 = window.performance.now()
-    const editor = new TextEditor({buffer, largeFileMode: true})
-    document.body.appendChild(editor.element)
-    const t1 = window.performance.now()
-    data.push({
-      name: 'Opening and rendering a TextEditor in large file mode',
-      x: lineCount, duration: t1 - t0
-    })
-    editor.element.remove()
-    editor.destroy()
-  }
-  return data
+  const text = 'Lorem ipsum dolor sit amet\n'.repeat(test ? 10 : 500000)
+  const t0 = window.performance.now()
+  const buffer = new TextBuffer(text)
+  const editor = new TextEditor({buffer, largeFileMode: true})
+  editor.element.style.height = "600px"
+  document.body.appendChild(editor.element)
+  const t1 = window.performance.now()
+  editor.element.remove()
+  editor.destroy()
+
+  return [{name: 'Opening and rendering a large file', duration: t1 - t0}]
 }
