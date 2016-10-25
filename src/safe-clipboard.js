0633630511ddd0c5ef04ecf2407c9223db08fcde
Machiste Quintana <mnquintana@users.noreply.github.com>
2016-10-24 23:16:13 -0700

    Convert SafeClipboard to JS

---
+++ src/safe-clipboard.js
@@ -0,0 +1,10 @@
+let clipboard
+
+// Using clipboard in renderer process is not safe on Linux.
+if (process.platform === 'linux' && process.type === 'renderer') {
+  clipboard = require('electron').remote.clipboard
+} else {
+  clipboard = require('electron').clipboard;
+}
+
+export default clipboard
