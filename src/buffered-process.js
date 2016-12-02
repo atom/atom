01de6855f182d0e6dd83e00b8b116736b1a15e9f
Damien Guard <damieng@gmail.com>
2016-12-02 13:20:07 -0800

    Properly escape &^| chars in commands on Windows. Fixes #13178

--- src/buffered-process.js
+++ src/buffered-process.js
@@ -51,38 +51,40 @@ export default class BufferedProcess {
     this.command = command
     // Related to joyent/node#2318
     if (process.platform === 'win32' && options.shell === undefined) {
-      let cmdArgs = []
-
-      // Quote all arguments and escapes inner quotes
-      if (args) {
-        cmdArgs = args.filter((arg) => arg != null)
-          .map((arg) => {
-            if (this.isExplorerCommand(command) && /^\/[a-zA-Z]+,.*$/.test(arg)) {
-              // Don't wrap /root,C:\folder style arguments to explorer calls in
-              // quotes since they will not be interpreted correctly if they are
-              return arg
-            } else {
-              return `\"${arg.toString().replace(/"/g, '\\"')}\"`
-            }
-          })
-      }
-
-      if (/\s/.test(command)) {
-        cmdArgs.unshift(`\"${command}\"`)
-      } else {
-        cmdArgs.unshift(command)
-      }
-
-      cmdArgs = ['/s', '/d', '/c', `\"${cmdArgs.join(' ')}\"`]
-      const cmdOptions = _.clone(options)
-      cmdOptions.windowsVerbatimArguments = true
-      this.spawn(this.getCmdPath(), cmdArgs, cmdOptions)
+      this.spawnWithEscapedWindowsArgs(command, args, options)
     } else {
       this.spawn(command, args, options)
     }
 
     this.killed = false
     this.handleEvents(stdout, stderr, exit)
+  }
+
+  // Windows has a bunch of special rules that node still doesn't take care of for you
+  spawnWithEscapedWindowsArgs (command, args, options) {
+    let cmdArgs = []
+    // Quote all arguments and escapes inner quotes
+    if (args) {
+      cmdArgs = args.filter((arg) => arg != null)
+        .map((arg) => {
+          if (this.isExplorerCommand(command) && /^\/[a-zA-Z]+,.*$/.test(arg)) {
+            // Don't wrap /root,C:\folder style arguments to explorer calls in
+            // quotes since they will not be interpreted correctly if they are
+            return arg
+          } else {
+            // Escape double quotes by putting a backslash in front of them
+            return `\"${arg.toString().replace(/"/g, '\\"')}\"`
+          }
+        })
+    }
+
+    // The command itself is quoted if it contains spaces, &, ^ or | chars
+    cmdArgs.unshift(/\s|&|\^|\|/.test(command) ? `\"${command}\"` : command)
+
+    const cmdOptions = _.clone(options)
+    cmdOptions.windowsVerbatimArguments = true
+
+    this.spawn(this.getCmdPath(), ['/s', '/d', '/c', `\"${cmdArgs.join(' ')}\"`], cmdOptions)
   }
 
   /*
