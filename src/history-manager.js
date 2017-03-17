d94c8811f39c9cad307d43451ef617dc7f045dee
Antonio Scandurra <as-cii@github.com>
2017-03-17 16:19:25 +0100

    Replace CommandRegistry.addBundled with a boolean param in .add

--- src/history-manager.js
+++ src/history-manager.js
@@ -13,7 +13,7 @@ export class HistoryManager {
     this.emitter = new Emitter()
     this.projects = []
     this.disposables = new CompositeDisposable()
-    this.disposables.add(commands.addBundled('atom-workspace', {'application:clear-project-history': this.clearProjects.bind(this)}))
+    this.disposables.add(commands.add('atom-workspace', {'application:clear-project-history': this.clearProjects.bind(this)}, false))
     this.disposables.add(project.onDidChangePaths((projectPaths) => this.addProject(projectPaths)))
   }
 
