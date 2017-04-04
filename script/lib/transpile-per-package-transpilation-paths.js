155c3a14bf450f42301316fb24c59b67700b0a84
Michelle Tilley <binarymuse@github.com>
2017-04-04 07:54:14 -0700

    Perform per-package transpilation on bundled packages during build

---
+++ script/lib/transpile-per-package-transpilation-paths.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const CompileCache = require('../../src/compile-cache')
+const fs = require('fs')
+const glob = require('glob')
+const path = require('path')
+
+const CONFIG = require('../config')
+
+module.exports = function () {
+  console.log(`Transpiling per-package transpilation paths in ${CONFIG.intermediateAppPath}`)
+  let pathsToCompile = []
+  for (let packageName of Object.keys(CONFIG.appMetadata.packageDependencies)) {
+    const packagePath = path.join(CONFIG.intermediateAppPath, 'node_modules', packageName)
+    const metadataPath = path.join(packagePath, 'package.json')
+    const metadata = require(metadataPath)
+    if (metadata.atomTranspilers) {
+      CompileCache.addTranspilerConfigForPath(packagePath, metadata.name, metadata, metadata.atomTranspilers)
+      for (let config of metadata.atomTranspilers) {
+        pathsToCompile = pathsToCompile.concat(glob.sync(path.join(packagePath, config.glob)))
+      }
+    }
+  }
+
+  for (let path of pathsToCompile) {
+    transpilePath(path)
+  }
+}
+
+function transpilePath (path) {
+  fs.writeFileSync(path, CompileCache.addPathToCache(path, CONFIG.atomHomeDirPath))
+}
