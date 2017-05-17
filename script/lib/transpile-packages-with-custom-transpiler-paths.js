6e61fe9a3235785f068bc692811ff1519792c719
Michelle Tilley <binarymuse@github.com>
2017-05-17 19:01:36 +0200

    Install devDeps in the root package path where they're not cached

--- script/lib/transpile-packages-with-custom-transpiler-paths.js
+++ script/lib/transpile-packages-with-custom-transpiler-paths.js
@@ -15,21 +15,33 @@ require('colors')
 module.exports = function () {
   console.log(`Transpiling packages with custom transpiler configurations in ${CONFIG.intermediateAppPath}`)
   for (let packageName of Object.keys(CONFIG.appMetadata.packageDependencies)) {
-    const packagePath = path.join(CONFIG.intermediateAppPath, 'node_modules', packageName)
-    const metadataPath = path.join(packagePath, 'package.json')
+    const rootPackagePath = path.join(CONFIG.repositoryRootPath, 'node_modules', packageName)
+    const intermediatePackagePath = path.join(CONFIG.intermediateAppPath, 'node_modules', packageName)
+
+    const metadataPath = path.join(intermediatePackagePath, 'package.json')
     const metadata = require(metadataPath)
+
     if (metadata.atomTranspilers) {
       console.log(' transpiling for package '.cyan + packageName.cyan)
-      const restoreNodeModules = backupNodeModules(packagePath)
-      runApmInstall(packagePath)
+      const rootPackageBackup = backupNodeModules(rootPackagePath)
+      const intermediatePackageBackup = backupNodeModules(intermediatePackagePath)
 
-      CompileCache.addTranspilerConfigForPath(packagePath, metadata.name, metadata, metadata.atomTranspilers)
+      // Run `apm install` in the *root* pacakge's path, so we get devDeps w/o apm's weird caching
+      // Then copy this folder into the intermediate package's path so we can run the transpilation in-line.
+      runApmInstall(rootPackagePath)
+      if (fs.existsSync(intermediatePackageBackup.nodeModulesPath)) {
+        fs.removeSync(intermediatePackageBackup.nodeModulesPath)
+      }
+      fs.copySync(rootPackageBackup.nodeModulesPath, intermediatePackageBackup.nodeModulesPath)
+
+      CompileCache.addTranspilerConfigForPath(intermediatePackagePath, metadata.name, metadata, metadata.atomTranspilers)
       for (let config of metadata.atomTranspilers) {
-        const pathsToCompile = glob.sync(path.join(packagePath, config.glob), {nodir: true})
+        const pathsToCompile = glob.sync(path.join(intermediatePackagePath, config.glob), {nodir: true})
         pathsToCompile.forEach(transpilePath)
       }
 
-      restoreNodeModules()
+      rootPackageBackup.restore()
+      intermediatePackageBackup.restore()
     }
   }
 }
