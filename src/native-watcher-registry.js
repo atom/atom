931b4e70554e64bbc2039de45b625af0ad1bf0f4
Ash Wilson <smashwilson@github.com>
2017-08-02 12:33:06 -0400

    :shirt: lint lint lint

--- src/native-watcher-registry.js
+++ src/native-watcher-registry.js
@@ -3,7 +3,7 @@
 const path = require('path')
 
 // Private: re-join the segments split from an absolute path to form another absolute path.
-function absolute(...parts) {
+function absolute (...parts) {
   const candidate = path.join(...parts)
   return path.isAbsolute(candidate) ? candidate : path.join(path.sep, candidate)
 }
