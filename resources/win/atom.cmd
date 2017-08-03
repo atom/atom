b5bdf4acb9f1fbd1775d94cc4d696cc396fcef7e
Nathan Sobo <nathan@github.com>
2017-08-02 21:03:29 -0600

    Don't enable electron logging by default

--- resources/win/atom.cmd
+++ resources/win/atom.cmd
@@ -5,16 +5,17 @@ SET WAIT=
 SET PSARGS=%*
 
 FOR %%a IN (%*) DO (
-  IF /I "%%a"=="-f"               SET EXPECT_OUTPUT=YES
-  IF /I "%%a"=="--foreground"     SET EXPECT_OUTPUT=YES
-  IF /I "%%a"=="-h"               SET EXPECT_OUTPUT=YES
-  IF /I "%%a"=="--help"           SET EXPECT_OUTPUT=YES
-  IF /I "%%a"=="-t"               SET EXPECT_OUTPUT=YES
-  IF /I "%%a"=="--test"           SET EXPECT_OUTPUT=YES
-  IF /I "%%a"=="--benchmark"      SET EXPECT_OUTPUT=YES
-  IF /I "%%a"=="--benchmark-test" SET EXPECT_OUTPUT=YES
-  IF /I "%%a"=="-v"               SET EXPECT_OUTPUT=YES
-  IF /I "%%a"=="--version"        SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="-f"                         SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="--foreground"               SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="-h"                         SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="--help"                     SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="-t"                         SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="--test"                     SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="--benchmark"                SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="--benchmark-test"           SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="-v"                         SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="--version"                  SET EXPECT_OUTPUT=YES
+  IF /I "%%a"=="--enable-electron-logging"  SET ELECTRON_ENABLE_LOGGING=YES
   IF /I "%%a"=="-w"           (
     SET EXPECT_OUTPUT=YES
     SET WAIT=YES
@@ -26,7 +27,6 @@ FOR %%a IN (%*) DO (
 )
 
 IF "%EXPECT_OUTPUT%"=="YES" (
-  SET ELECTRON_ENABLE_LOGGING=YES
   IF "%WAIT%"=="YES" (
     powershell -noexit "Start-Process -FilePath \"%~dp0\..\..\atom.exe\" -ArgumentList \"--pid=$pid $env:PSARGS\" ; wait-event"
     exit 0
