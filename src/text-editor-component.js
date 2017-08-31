71bc18b63dd5c4bc473a58971ec0ac9157e5a5f4
Antonio Scandurra <as-cii@github.com>
2017-08-31 14:29:25 +0200

    Merge pull request #15515 from atom/as-dont-move-cursor-when-clicking-on-scrollbar
    Don't move the cursor when clicking on a scrollbar

--- src/text-editor-component.js
+++ src/text-editor-component.js
@@ -2975,7 +2975,7 @@ class DummyScrollbarComponent {
     const {
       orientation, scrollWidth, scrollHeight,
       verticalScrollbarWidth, horizontalScrollbarHeight,
-      canScroll, forceScrollbarVisible, didScroll, didMouseDown
+      canScroll, forceScrollbarVisible, didScroll
     } = this.props
 
     const outerStyle = {
@@ -3017,7 +3017,7 @@ class DummyScrollbarComponent {
         style: outerStyle,
         on: {
           scroll: didScroll,
-          mousedown: didMouseDown
+          mousedown: this.didMouseDown
         }
       },
       $.div({style: innerStyle})
