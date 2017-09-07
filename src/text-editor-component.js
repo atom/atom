32623bdb19b64ca820671c55436dc5083d8ea59d
Antonio Scandurra <as-cii@github.com>
2017-09-07 16:39:16 +0200

    Merge pull request #15580 from atom/as-update-scrollbars-on-detach-reattach
    Flush scroll position to dummy scrollbar components on re-attach

--- src/text-editor-component.js
+++ src/text-editor-component.js
@@ -1426,6 +1426,9 @@ class TextEditorComponent {
 
       if (this.isVisible()) {
         this.didShow()
+
+        if (this.refs.verticalScrollbar) this.refs.verticalScrollbar.flushScrollPosition()
+        if (this.refs.horizontalScrollbar) this.refs.horizontalScrollbar.flushScrollPosition()
       } else {
         this.didHide()
       }
