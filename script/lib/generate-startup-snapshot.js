aeb8db2e14640b738a006f5f90a315cfc0923c14
Nathan Sobo <nathan@github.com>
2017-12-21 10:10:12 -0700

    Don't snapshot lodash.isequal because it builds an ArrayBuffer on eval
    This won't work until v8 6.2.

--- script/lib/generate-startup-snapshot.js
+++ script/lib/generate-startup-snapshot.js
@@ -46,6 +46,7 @@ module.exports = function (packagedAppPath) {
         relativePath === path.join('..', 'node_modules', 'less', 'index.js') ||
         relativePath === path.join('..', 'node_modules', 'less', 'lib', 'less', 'fs.js') ||
         relativePath === path.join('..', 'node_modules', 'less', 'lib', 'less-node', 'index.js') ||
+        relativePath === path.join('..', 'node_modules', 'lodash.isequal', 'index.js') ||
         relativePath === path.join('..', 'node_modules', 'node-fetch', 'lib', 'fetch-error.js') ||
         relativePath === path.join('..', 'node_modules', 'superstring', 'index.js') ||
         relativePath === path.join('..', 'node_modules', 'oniguruma', 'src', 'oniguruma.js') ||
