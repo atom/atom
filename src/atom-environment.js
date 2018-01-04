75b4a7a984dc7afe83da12cc884acd46216c4417
Max Brunsfeld <maxbrunsfeld@gmail.com>
2018-01-04 12:12:21 -0800

    :arrow_up: joanna, put back AtomEnvironment public property docs

--- src/atom-environment.js
+++ src/atom-environment.js
@@ -51,13 +51,15 @@ let nextId = 0
 //
 // An instance of this class is always available as the `atom` global.
 class AtomEnvironment {
+
   /*
-  Section: Construction and Destruction
+  Section: Properties
   */
 
-  // Call .loadOrCreate instead
   constructor (params = {}) {
     this.id = (params.id != null) ? params.id : nextId++
+
+    // Public: A {Clipboard} instance
     this.clipboard = params.clipboard
     this.updateProcessEnv = params.updateProcessEnv || updateProcessEnv
     this.enablePersistence = params.enablePersistence
@@ -68,25 +70,43 @@ class AtomEnvironment {
     this.loadTime = null
     this.emitter = new Emitter()
     this.disposables = new CompositeDisposable()
+
+    // Public: A {DeserializerManager} instance
     this.deserializers = new DeserializerManager(this)
     this.deserializeTimings = {}
+
+    // Public: A {ViewRegistry} instance
     this.views = new ViewRegistry(this)
+
+    // Public: A {NotificationManager} instance
     this.notifications = new NotificationManager()
 
     this.stateStore = new StateStore('AtomEnvironments', 1)
 
+    // Public: A {Config} instance
     this.config = new Config({
       notificationManager: this.notifications,
       enablePersistence: this.enablePersistence
     })
     this.config.setSchema(null, {type: 'object', properties: _.clone(ConfigSchema)})
 
+    // Public: A {KeymapManager} instance
     this.keymaps = new KeymapManager({notificationManager: this.notifications})
+
+    // Public: A {TooltipManager} instance
     this.tooltips = new TooltipManager({keymapManager: this.keymaps, viewRegistry: this.views})
+
+    // Public: A {CommandRegistry} instance
     this.commands = new CommandRegistry()
     this.uriHandlerRegistry = new URIHandlerRegistry()
+
+    // Public: A {GrammarRegistry} instance
     this.grammars = new GrammarRegistry({config: this.config})
+
+    // Public: A {StyleManager} instance
     this.styles = new StyleManager()
+
+    // Public: A {PackageManager} instance
     this.packages = new PackageManager({
       config: this.config,
       styleManager: this.styles,
@@ -98,6 +118,8 @@ class AtomEnvironment {
       viewRegistry: this.views,
       uriHandlerRegistry: this.uriHandlerRegistry
     })
+
+    // Public: A {ThemeManager} instance
     this.themes = new ThemeManager({
       packageManager: this.packages,
       config: this.config,
@@ -105,12 +127,18 @@ class AtomEnvironment {
       notificationManager: this.notifications,
       viewRegistry: this.views
     })
+
+    // Public: A {MenuManager} instance
     this.menu = new MenuManager({keymapManager: this.keymaps, packageManager: this.packages})
+
+    // Public: A {ContextMenuManager} instance
     this.contextMenu = new ContextMenuManager({keymapManager: this.keymaps})
+
     this.packages.setMenuManager(this.menu)
     this.packages.setContextMenuManager(this.contextMenu)
     this.packages.setThemeManager(this.themes)
 
+    // Public: A {Project} instance
     this.project = new Project({
       notificationManager: this.notifications,
       packageManager: this.packages,
@@ -121,6 +149,7 @@ class AtomEnvironment {
     this.commandInstaller = new CommandInstaller(this.applicationDelegate)
     this.protocolHandlerInstaller = new ProtocolHandlerInstaller()
 
+    // Public: A {TextEditorRegistry} instance
     this.textEditors = new TextEditorRegistry({
       config: this.config,
       grammarRegistry: this.grammars,
@@ -128,6 +157,7 @@ class AtomEnvironment {
       packageManager: this.packages
     })
 
+    // Public: A {Workspace} instance
     this.workspace = new Workspace({
       config: this.config,
       project: this.project,
@@ -157,7 +187,9 @@ class AtomEnvironment {
 
     this.windowEventHandler = new WindowEventHandler({atomEnvironment: this, applicationDelegate: this.applicationDelegate})
 
+    // Public: A {HistoryManager} instance
     this.history = new HistoryManager({project: this.project, commands: this.commands, stateStore: this.stateStore})
+
     // Keep instances of HistoryManager in sync
     this.disposables.add(this.history.onDidChangeProjects(event => {
       if (!event.reloaded) this.applicationDelegate.didChangeHistoryManager()
