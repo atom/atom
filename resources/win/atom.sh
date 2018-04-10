f1c03c81632bb1423224684b0e82f4b50b55153d
Elijah Madden <okonomiyaki3000@gmail.com>
2018-04-10 14:24:53 +0900

    Convert arguments to absolute paths
    This script launches cmd and has it launch atom with the passed arguments. The problem is that cmd will not (necessarily) start in the pwd. So, relative paths like `.` and `..` have a different meaning. So we should first convert them to absolute paths with cygpath. 
    
    Try it yourself. Go to some directory in bash and run `atom .` Rather than opening your current bash pwd, you'll open $HOME because when cmd opens, that's where it will be. Now apply this change and try the same thing. 
    
    By the way, I only see this problem affecting MSysGit and MSys2. I can't say whether it affects other environments.

--- resources/win/atom.sh
+++ resources/win/atom.sh
@@ -3,7 +3,9 @@
 if command -v "cygpath" > /dev/null; then
   # We have cygpath to do the conversion
   ATOMCMD=$(cygpath "$(dirname "$0")/atom.cmd" -a -w)
+  ARGS=( $(cygpath -a -w "$@" | tr '\n' ' ') ) 
 else
+  ARGS=$@
   pushd "$(dirname "$0")" > /dev/null
   if [[ $(uname -r) == *-Microsoft ]]; then
     # We are in Windows Subsystem for Linux, map /mnt/drive
@@ -16,7 +18,7 @@ else
   popd > /dev/null
 fi
 if [ "$(uname -o)" == "Msys" ]; then
-  cmd.exe //C "$ATOMCMD" "$@" # Msys thinks /C is a Windows path...
+  cmd.exe //C "$ATOMCMD" "${ARGS[@]}" # Msys thinks /C is a Windows path...
 else
-  cmd.exe /C "$ATOMCMD" "$@" # Cygwin does not
+  cmd.exe /C "$ATOMCMD" "${ARGS[@]}" # Cygwin does not
 fi
