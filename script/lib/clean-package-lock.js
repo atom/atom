f759bed88281f5492b9e2e6e33cb9edcef675d92
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-07-11 19:54:29 -0700

    Don't delete the package-lock.json in /script/vsts

--- script/lib/clean-package-lock.js
+++ script/lib/clean-package-lock.js
@@ -10,7 +10,7 @@ const path = require('path')
 
 module.exports = function () {
   console.log('Deleting problematic package-lock.json files')
-  let paths = glob.sync(path.join(CONFIG.repositoryRootPath, '**', 'package-lock.json'), {ignore: path.join('**', 'node_modules', '**')})
+  let paths = glob.sync(path.join(CONFIG.repositoryRootPath, '**', 'package-lock.json'), {ignore: [path.join('**', 'node_modules', '**'), path.join('**', 'vsts', '**')]})
 
   for (let path of paths) {
     fs.unlinkSync(path)
