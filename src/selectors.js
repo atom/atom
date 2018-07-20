be0565f3f4acfb9a50b1e9bb846e63327a1d3cf3
Ashi Krishnan <queerviolet@github.com>
2018-07-20 18:47:46 -0400

    Convert to atom doc

--- src/selectors.js
+++ src/selectors.js
@@ -1,14 +1,12 @@
 module.exports = {selectorMatchesAnyScope, matcherForSelector}
 
-const _ = require('underscore-plus')
+const {isSubset} = require('underscore-plus')
 
-/**
- * Parse a selector into parts. If already parsed, returns the selector
- * unmodified.
- * 
- * @param {String|Array<String>} selector
- * @returns {Array<String>} selector parts
- */
+// Private: Parse a selector into parts.
+//          If already parsed, returns the selector unmodified.
+// 
+// * `selector` a {String|Array<String>} specifying what to match
+// Returns selector parts, an {Array<String>}.
 function parse (selector) {
   return typeof selector === 'string'
     ? selector.replace(/^\./, '').split('.')
@@ -17,26 +15,23 @@ function parse (selector) {
 
 const always = scope => true
 
-/**
- * Return a matcher function for a selector.
- * 
- * @param {String} selector
- * @returns {(scope: String) -> Boolean} a matcher function
- */
+// Essential: Return a matcher function for a selector.
+//
+// * selector, a {String} selector
+// Returns {(scope: String) -> Boolean}, a matcher function returning
+// true iff the scope matches the selector.
 function matcherForSelector (selector) {
   const parts = parse(selector)
   return selector
-    ? scope => _.isSubset(parts, parse(scope))
+    ? scope => isSubset(parts, parse(scope))
     : always
 }
 
-/**
- * Return true iff the selector matches any provided scope.
- * 
- * @param {String} selector
- * @param {Array<String>} scopes
- * @returns {Boolean} true if any scope matches the selector
- */
+// Essential: Return true iff the selector matches any provided scope.
+//
+// * {String} selector
+// * {Array<String>} scopes
+// Returns {Boolean} true if any scope matches the selector.
 function selectorMatchesAnyScope (selector, scopes) {
   return !selector || scopes.some(matcherForSelector(selector))
 }
