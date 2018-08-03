5bdc2babde98b0d3074f33204d4aeab192a3ca33
David Wilson <daviwil@github.com>
2018-08-03 06:37:04 -0700

    Fix missing 7zip on Windows VSTS builds

--- script/lib/compress-artifacts.js
+++ script/lib/compress-artifacts.js
@@ -3,6 +3,7 @@
 const fs = require('fs-extra')
 const path = require('path')
 const spawnSync = require('./spawn-sync')
+const { path7za } = require('7zip-bin')
 
 const CONFIG = require('../config')
 
@@ -44,7 +45,7 @@ function compress (inputDirPath, outputArchivePath) {
     compressCommand = 'zip'
     compressArguments = ['-r', '--symlinks']
   } else if (process.platform === 'win32') {
-    compressCommand = '7z.exe'
+    compressCommand = path7za
     compressArguments = ['a', '-r']
   } else {
     compressCommand = 'tar'
