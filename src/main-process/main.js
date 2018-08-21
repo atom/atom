ba12a1e2fc12543ff49d1c9add6b997086574961
David Wilson <daviwil@github.com>
2018-08-21 11:57:18 -0700

    Simplify dev resource path detection

--- src/main-process/main.js
+++ src/main-process/main.js
@@ -5,8 +5,10 @@ if (typeof snapshotResult !== 'undefined') {
 const startTime = Date.now()
 
 const path = require('path')
+const fs = require('fs-plus')
+const CSON = require('season')
 const yargs = require('yargs')
-const getDevResourcePath = require('./get-dev-resource-path')
+const electron = require('electron')
 
 const args =
   yargs(process.argv)
@@ -14,14 +16,34 @@ const args =
     .alias('t', 'test')
     .argv
 
+function isAtomRepoPath (repoPath) {
+  let packageJsonPath = path.join(repoPath, 'package.json')
+  if (fs.statSyncNoException(packageJsonPath)) {
+    let packageJson = CSON.readFileSync(packageJsonPath)
+    return packageJson.name === 'atom'
+  }
+
+  return false
+}
+
 let resourcePath
 
 if (args.resourcePath) {
   resourcePath = args.resourcePath
 } else {
   const stableResourcePath = path.dirname(path.dirname(__dirname))
+  const defaultRepositoryPath = path.join(electron.app.getPath('home'), 'github', 'atom')
+
   if (args.dev || args.test || args.benchmark || args.benchmarkTest) {
-    resourcePath = getDevResourcePath() || stableResourcePath
+    if (process.env.ATOM_DEV_RESOURCE_PATH) {
+      resourcePath = process.env.ATOM_DEV_RESOURCE_PATH
+    } else if (isAtomRepoPath(process.cwd())) {
+      resourcePath = process.cwd()
+    } else if (fs.statSyncNoException(defaultRepositoryPath)) {
+      resourcePath = defaultRepositoryPath
+    } else {
+      resourcePath = stableResourcePath
+    }
   } else {
     resourcePath = stableResourcePath
   }
