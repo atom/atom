51044ce16e01c67b98e8d0ff8da5ce9318e6bb1c
Max Brunsfeld <maxbrunsfeld@gmail.com>
2018-08-24 12:00:44 -0700

    Avoid babel in test files that are run in the main process

--- spec/main-process/parse-command-line.test.js
+++ spec/main-process/parse-command-line.test.js
@@ -1,10 +1,8 @@
-/** @babel */
+const parseCommandLine = require('../../src/main-process/parse-command-line')
 
-import parseCommandLine from '../../src/main-process/parse-command-line'
-
-describe('parseCommandLine', function () {
-  describe('when --uri-handler is not passed', function () {
-    it('parses arguments as normal', function () {
+describe('parseCommandLine', () => {
+  describe('when --uri-handler is not passed', () => {
+    it('parses arguments as normal', () => {
       const args = parseCommandLine(['-d', '--safe', '--test', '/some/path', 'atom://test/url', 'atom://other/url'])
       assert.isTrue(args.devMode)
       assert.isTrue(args.safeMode)
@@ -14,8 +12,8 @@ describe('parseCommandLine', function () {
     })
   })
 
-  describe('when --uri-handler is passed', function () {
-    it('ignores other arguments and limits to one URL', function () {
+  describe('when --uri-handler is passed', () => {
+    it('ignores other arguments and limits to one URL', () => {
       const args = parseCommandLine(['-d', '--uri-handler', '--safe', '--test', '/some/path', 'atom://test/url', 'atom://other/url'])
       assert.isUndefined(args.devMode)
       assert.isUndefined(args.safeMode)
