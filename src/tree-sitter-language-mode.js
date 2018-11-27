a34e2e8d0a738968a4403b0083873118a41ce3c8
Max Brunsfeld <maxbrunsfeld@github.com>
2018-11-27 11:11:33 -0800

    Merge pull request #18502 from atom/mb-fold-marker-updates
    Fix typo that messed up fold marker updates

--- src/tree-sitter-language-mode.js
+++ src/tree-sitter-language-mode.js
@@ -94,7 +94,7 @@ class TreeSitterLanguageMode {
     }
   }
 
-  bufferDidFinishTransaction (changes) {
+  bufferDidFinishTransaction ({changes}) {
     for (let i = 0, {length} = changes; i < length; i++) {
       const {oldRange, newRange} = changes[i]
       spliceArray(
