501063b1bdde2ed4f5d0132201159e2996378f81
Ash Wilson <smashwilson@github.com>
2019-02-11 16:13:29 -0500

    That method was renamed to pathExists

--- script/lib/clean-output-directory.js
+++ script/lib/clean-output-directory.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra')
 const CONFIG = require('../config')
 
 module.exports = async function () {
-  if (await fs.exists(CONFIG.buildOutputPath)) {
+  if (await fs.pathExists(CONFIG.buildOutputPath)) {
     console.log(`Cleaning ${CONFIG.buildOutputPath}`)
     await fs.remove(CONFIG.buildOutputPath)
   }
