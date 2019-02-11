2c5fac289970013f80bb910c1032c2dd15944100
Ash Wilson <smashwilson@github.com>
2019-02-11 16:13:41 -0500

    Don't copy src/ and vendor/ to out/

--- script/lib/copy-assets.js
+++ script/lib/copy-assets.js
@@ -16,11 +16,8 @@ module.exports = async function () {
     path.join(CONFIG.repositoryRootPath, 'dot-atom'),
     path.join(CONFIG.repositoryRootPath, 'exports'),
     path.join(CONFIG.repositoryRootPath, 'package.json'),
-    path.join(CONFIG.repositoryRootPath, 'static'),
-    path.join(CONFIG.repositoryRootPath, 'src'),
-    path.join(CONFIG.repositoryRootPath, 'vendor')
+    path.join(CONFIG.repositoryRootPath, 'static')
   ]
-  srcPaths = srcPaths.concat(glob.sync(path.join(CONFIG.repositoryRootPath, 'spec', '*.*'), {ignore: path.join('**', '*-spec.*')}))
 
   await Promise.all(
     srcPaths.map(srcPath => fs.copy(srcPath, computeDestinationPath(srcPath), {filter: includePathInPackagedApp}))
@@ -31,16 +28,15 @@ module.exports = async function () {
   // copied to the output folder correctly.  We dereference only the top-level
   // symlinks and not nested symlinks to avoid issues where symlinked binaries
   // are duplicated in Atom's installation packages (see atom/atom#18490).
-  const nodeModulesPath = path.join(CONFIG.repositoryRootPath, 'node_modules')
-  await Promise.all(
-    glob.sync(path.join(nodeModulesPath, '*')).map(async p => {
-      const stat = await fs.lstat(p)
-      if (!stat) { console.log(p) }
-      const modulePath = stat.isSymbolicLink() ? path.resolve(nodeModulesPath, await fs.readlink(p)) : p
-      const destPath = path.join(CONFIG.intermediateAppPath, 'node_modules', path.basename(modulePath))
-      await fs.copy(modulePath, destPath, { filter: includePathInPackagedApp })
-    })
-  )
+  // const nodeModulesPath = path.join(CONFIG.repositoryRootPath, 'node_modules')
+  // await Promise.all(
+  //   glob.sync(path.join(nodeModulesPath, '*')).map(async p => {
+  //     const stat = await fs.lstat(p)
+  //     const modulePath = stat.isSymbolicLink() ? path.resolve(nodeModulesPath, await fs.readlink(p)) : p
+  //     const destPath = path.join(CONFIG.intermediateAppPath, 'node_modules', path.basename(modulePath))
+  //     await fs.copy(modulePath, destPath, { filter: includePathInPackagedApp })
+  //   })
+  // )
 
   await fs.copy(
     path.join(CONFIG.repositoryRootPath, 'resources', 'app-icons', CONFIG.channel, 'png', '1024.png'),
