f701d1437a468b41d9314feadce2f31c74aeb835
Ash Wilson <smashwilson@gmail.com>
2019-02-16 20:11:35 -0500

    :shirt: standard

--- spec/path-watcher-spec.js
+++ spec/path-watcher-spec.js
@@ -1,12 +1,12 @@
 /** @babel */
 
-import {it, beforeEach, afterEach, promisifySome} from './async-spec-helpers'
+import { it, beforeEach, afterEach, promisifySome } from './async-spec-helpers'
 import tempCb from 'temp'
 import fsCb from 'fs-plus'
 import path from 'path'
 
-import {CompositeDisposable} from 'event-kit'
-import {watchPath, stopAllWatchers} from '../src/path-watcher'
+import { CompositeDisposable } from 'event-kit'
+import { watchPath, stopAllWatchers } from '../src/path-watcher'
 
 tempCb.track()
 
@@ -14,23 +14,23 @@ const fs = promisifySome(fsCb, ['writeFile', 'mkdir', 'symlink', 'appendFile', '
 const temp = promisifySome(tempCb, ['mkdir'])
 
 class ScopedLogger {
-  constructor () {
+  constructor() {
     this.messages = []
     this.iteration = null
   }
 
-  enable (iteration) {
+  enable(iteration) {
     this.iteration = iteration
   }
 
-  log (message) {
+  log(message) {
     if (this.iteration === null) {
       return
     }
     this.messages.push(message)
   }
 
-  async dump () {
+  async dump() {
     if (this.iteration === null) {
       return
     }
@@ -38,26 +38,26 @@ class ScopedLogger {
     const fileName = path.resolve('logs', baseName)
     const content = this.messages.map(line => line + '\n').join('')
     this.iteration = null
-    await fs.writeFile(fileName, content, {encoding: 'utf8'})
+    await fs.writeFile(fileName, content, { encoding: 'utf8' })
   }
 }
 
-describe('watchPath', function () {
+describe('watchPath', function() {
   let subs, logger
 
-  beforeEach(function () {
+  beforeEach(function() {
     logger = new ScopedLogger()
     subs = new CompositeDisposable()
   })
 
-  afterEach(function () {
+  afterEach(function() {
     subs.dispose()
 
     waitsForPromise(logger.dump())
     waitsForPromise(stopAllWatchers())
   })
 
-  function waitForChanges (watcher, ...fileNames) {
+  function waitForChanges(watcher, ...fileNames) {
     const waiting = new Set(fileNames)
     let fired = false
     const relevantEvents = []
@@ -89,48 +89,48 @@ describe('watchPath', function () {
     })
   }
 
-  describe('watchPath()', function () {
-    it('resolves the returned promise when the watcher begins listening', async function () {
+  describe('watchPath()', function() {
+    it('resolves the returned promise when the watcher begins listening', async function() {
       const rootDir = await temp.mkdir('atom-fsmanager-test-')
 
-      const watcher = await watchPath(rootDir, {}, () => {})
+      const watcher = await watchPath(rootDir, {}, () => { })
       expect(watcher.constructor.name).toBe('PathWatcher')
     })
 
-    it('reuses an existing native watcher and resolves getStartPromise immediately if attached to a running watcher', async function () {
+    it('reuses an existing native watcher and resolves getStartPromise immediately if attached to a running watcher', async function() {
       const rootDir = await temp.mkdir('atom-fsmanager-test-')
 
-      const watcher0 = await watchPath(rootDir, {}, () => {})
-      const watcher1 = await watchPath(rootDir, {}, () => {})
+      const watcher0 = await watchPath(rootDir, {}, () => { })
+      const watcher1 = await watchPath(rootDir, {}, () => { })
 
       expect(watcher0.native).toBe(watcher1.native)
     })
 
-    it("reuses existing native watchers even while they're still starting", async function () {
+    it("reuses existing native watchers even while they're still starting", async function() {
       const rootDir = await temp.mkdir('atom-fsmanager-test-')
 
       const [watcher0, watcher1] = await Promise.all([
-        watchPath(rootDir, {}, () => {}),
-        watchPath(rootDir, {}, () => {})
+        watchPath(rootDir, {}, () => { }),
+        watchPath(rootDir, {}, () => { })
       ])
       expect(watcher0.native).toBe(watcher1.native)
     })
 
-    it("doesn't attach new watchers to a native watcher that's stopping", async function () {
+    it("doesn't attach new watchers to a native watcher that's stopping", async function() {
       const rootDir = await temp.mkdir('atom-fsmanager-test-')
 
-      const watcher0 = await watchPath(rootDir, {}, () => {})
+      const watcher0 = await watchPath(rootDir, {}, () => { })
       const native0 = watcher0.native
 
       watcher0.dispose()
-      const watcher1 = await watchPath(rootDir, {}, () => {})
+      const watcher1 = await watchPath(rootDir, {}, () => { })
 
       expect(watcher1.native).not.toBe(native0)
     })
 
     fdescribe('the flaking test', function() {
       for (let i = 0; i < 100; i++) {
-        it(`reuses an existing native watcher on a parent directory and filters events: ${i}`, async function () {
+        it(`reuses an existing native watcher on a parent directory and filters events: ${i}`, async function() {
           logger.enable(i)
           logger.log('start')
 
@@ -147,9 +147,9 @@ describe('watchPath', function () {
           // Keep the watchers alive with an undisposed subscription
           logger.log('creating watchers: start')
           logger.log(`watching: [${rootDir}]`)
-          const rootWatcher = await watchPath(rootDir, {}, () => {})
+          const rootWatcher = await watchPath(rootDir, {}, () => { })
           logger.log(`watching: [${subDir}]`)
-          const childWatcher = await watchPath(subDir, {}, () => {})
+          const childWatcher = await watchPath(subDir, {}, () => { })
           logger.log('creating watchers: done')
 
           expect(rootWatcher.native).toBe(childWatcher.native)
@@ -163,7 +163,7 @@ describe('watchPath', function () {
           logger.log('creating promise for first changes: done')
 
           logger.log(`writing to ${subFile}: start`)
-          await fs.writeFile(subFile, 'subfile\n', {encoding: 'utf8'})
+          await fs.writeFile(subFile, 'subfile\n', { encoding: 'utf8' })
           logger.log(`writing to ${subFile}: done`)
           logger.log(`await promise for first changes: start`)
           await firstChanges
@@ -173,7 +173,7 @@ describe('watchPath', function () {
           const nextRootEvent = waitForChanges(rootWatcher, rootFile)
           logger.log('creating promise for root changes: done')
           logger.log(`writing to ${rootFile}: start`)
-          await fs.writeFile(rootFile, 'rootfile\n', {encoding: 'utf8'})
+          await fs.writeFile(rootFile, 'rootfile\n', { encoding: 'utf8' })
           logger.log(`writing to ${rootFile}: done`)
           logger.log('await promise for root changes: start')
           await nextRootEvent
@@ -184,7 +184,7 @@ describe('watchPath', function () {
       }
     })
 
-    it('adopts existing child watchers and filters events appropriately to them', async function () {
+    it('adopts existing child watchers and filters events appropriately to them', async function() {
       const parentDir = await temp.mkdir('atom-fsmanager-test-').then(fs.realpath)
 
       // Create the directory tree
@@ -197,22 +197,22 @@ describe('watchPath', function () {
       await fs.mkdir(subDir0)
       await fs.mkdir(subDir1)
       await Promise.all([
-        fs.writeFile(rootFile, 'rootfile\n', {encoding: 'utf8'}),
-        fs.writeFile(subFile0, 'subfile 0\n', {encoding: 'utf8'}),
-        fs.writeFile(subFile1, 'subfile 1\n', {encoding: 'utf8'})
+        fs.writeFile(rootFile, 'rootfile\n', { encoding: 'utf8' }),
+        fs.writeFile(subFile0, 'subfile 0\n', { encoding: 'utf8' }),
+        fs.writeFile(subFile1, 'subfile 1\n', { encoding: 'utf8' })
       ])
 
       // Begin the child watchers and keep them alive
-      const subWatcher0 = await watchPath(subDir0, {}, () => {})
+      const subWatcher0 = await watchPath(subDir0, {}, () => { })
       const subWatcherChanges0 = waitForChanges(subWatcher0, subFile0)
 
-      const subWatcher1 = await watchPath(subDir1, {}, () => {})
+      const subWatcher1 = await watchPath(subDir1, {}, () => { })
       const subWatcherChanges1 = waitForChanges(subWatcher1, subFile1)
 
       expect(subWatcher0.native).not.toBe(subWatcher1.native)
 
       // Create the parent watcher
-      const parentWatcher = await watchPath(parentDir, {}, () => {})
+      const parentWatcher = await watchPath(parentDir, {}, () => { })
       const parentWatcherChanges = waitForChanges(parentWatcher, rootFile, subFile0, subFile1)
 
       expect(subWatcher0.native).toBe(parentWatcher.native)
@@ -220,9 +220,9 @@ describe('watchPath', function () {
 
       // Ensure events are filtered correctly
       await Promise.all([
-        fs.appendFile(rootFile, 'change\n', {encoding: 'utf8'}),
-        fs.appendFile(subFile0, 'change\n', {encoding: 'utf8'}),
-        fs.appendFile(subFile1, 'change\n', {encoding: 'utf8'})
+        fs.appendFile(rootFile, 'change\n', { encoding: 'utf8' }),
+        fs.appendFile(subFile0, 'change\n', { encoding: 'utf8' }),
+        fs.appendFile(subFile1, 'change\n', { encoding: 'utf8' })
       ])
 
       await Promise.all([
