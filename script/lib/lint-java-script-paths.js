8daaf3834eaa09e1b962d42984930e1bc8a357a8
Rafael Oleza <rafeca@github.com>
2019-02-22 12:19:52 +0100

    Enable linter on spec/ folder

--- script/lib/lint-java-script-paths.js
+++ script/lib/lint-java-script-paths.js
@@ -6,29 +6,46 @@ const path = require('path')
 
 const CONFIG = require('../config')
 
-module.exports = function () {
+module.exports = async function () {
   const globPathsToLint = [
     path.join(CONFIG.repositoryRootPath, 'exports', '**', '*.js'),
     path.join(CONFIG.repositoryRootPath, 'packages', '**', '*.js'),
     path.join(CONFIG.repositoryRootPath, 'script', '**', '*.js'),
+    path.join(CONFIG.repositoryRootPath, 'spec', '**', '*.js'),
     path.join(CONFIG.repositoryRootPath, 'src', '**', '*.js'),
     path.join(CONFIG.repositoryRootPath, 'static', '*.js')
   ]
-  return expandGlobPaths(globPathsToLint).then((paths) => {
-    return new Promise((resolve, reject) => {
-      standard.lintFiles(paths, (error, lintOutput) => {
-        if (error) {
-          reject(error)
-        } else {
-          const errors = []
-          for (let result of lintOutput.results) {
-            for (let message of result.messages) {
-              errors.push({path: result.filePath, lineNumber: message.line, message: message.message, rule: message.ruleId})
-            }
+  const globPathsToIgnore = [
+    path.join(CONFIG.repositoryRootPath, 'spec', 'fixtures', '**', '*.js')
+  ]
+
+  const [includePaths, excludePaths] = await Promise.all([
+    expandGlobPaths(globPathsToLint),
+    expandGlobPaths(globPathsToIgnore)
+  ])
+
+  const paths = includePaths.filter(
+    myPath => excludePaths.indexOf(myPath) === -1
+  )
+
+  return new Promise((resolve, reject) => {
+    standard.lintFiles(paths, (error, lintOutput) => {
+      if (error) {
+        reject(error)
+      } else {
+        const errors = []
+        for (let result of lintOutput.results) {
+          for (let message of result.messages) {
+            errors.push({
+              path: result.filePath,
+              lineNumber: message.line,
+              message: message.message,
+              rule: message.ruleId
+            })
           }
-          resolve(errors)
         }
-      })
+        resolve(errors)
+      }
     })
   })
 }
