540fd3761b09c4ed5d978b21e332bc6727af87a8
Winston Liu <50Wliu@users.noreply.github.com>
2019-03-20 16:42:43 -0400

    :art:

--- spec/clipboard-spec.js
+++ spec/clipboard-spec.js
@@ -1,12 +1,12 @@
-describe("Clipboard", () => {
-  describe("write(text, metadata) and read()", () => {
-    it("writes and reads text to/from the native clipboard", () => {
+describe('Clipboard', () => {
+  describe('write(text, metadata) and read()', () => {
+    it('writes and reads text to/from the native clipboard', () => {
       expect(atom.clipboard.read()).toBe('initial clipboard content')
       atom.clipboard.write('next')
       expect(atom.clipboard.read()).toBe('next')
     })
 
-    it("returns metadata if the item on the native clipboard matches the last written item", () => {
+    it('returns metadata if the item on the native clipboard matches the last written item', () => {
       atom.clipboard.write('next', {meta: 'data'})
       expect(atom.clipboard.read()).toBe('next')
       expect(atom.clipboard.readWithMetadata().text).toBe('next')
@@ -14,7 +14,7 @@ describe("Clipboard", () => {
     })
   })
 
-  describe("line endings", () => {
+  describe('line endings', () => {
     let originalPlatform = process.platform
 
     const eols = new Map([['win32', '\r\n'], ['darwin', '\n'], ['linux', '\n']])
