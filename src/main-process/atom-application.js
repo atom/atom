b903dc2506d0ab7b9f171f6e48de35f67d1e867b
Ash Wilson <smashwilson@github.com>
2019-04-18 22:56:12 -0400

    Save .projectRoots, not .initialProjectRoots which is undefined

--- src/main-process/atom-application.js
+++ src/main-process/atom-application.js
@@ -1123,7 +1123,7 @@ class AtomApplication extends EventEmitter {
 
     const states = []
     for (let window of this.getAllWindows()) {
-      if (!window.isSpec) states.push({initialPaths: window.initialProjectRoots})
+      if (!window.isSpec) states.push({initialPaths: window.projectRoots})
     }
     states.reverse()
 
