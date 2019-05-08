a6eabb2ae47b0a5226e192d084e6167e9f14de94
Winston Liu <50Wliu@users.noreply.github.com>
2019-05-07 20:35:25 -0400

    Log `apm install` output

--- script/lib/install-apm.js
+++ script/lib/install-apm.js
@@ -7,9 +7,9 @@ const CONFIG = require('../config')
 module.exports = function (ci) {
   console.log('Installing apm')
   // npm ci leaves apm with a bunch of unmet dependencies
-  childProcess.execFileSync(
+  console.log(childProcess.execFileSync(
     CONFIG.getNpmBinPath(),
-    ['--global-style', '--loglevel=error', 'install'],
+    ['--global-style', '--loglevel=silly', 'install'],
     {env: process.env, cwd: CONFIG.apmRootPath}
-  )
+  ).toString())
 }
