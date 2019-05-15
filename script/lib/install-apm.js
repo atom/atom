ad8a4e135b7b8eb218bd86d24239c9aef52540a8
Winston Liu <50Wliu@users.noreply.github.com>
2019-05-14 20:59:06 -0400

    Stick with --global-style

--- script/lib/install-apm.js
+++ script/lib/install-apm.js
@@ -8,7 +8,7 @@ module.exports = function (ci) {
   console.log('Installing apm')
   childProcess.execFileSync(
     CONFIG.getNpmBinPath(ci),
-    ['--loglevel=error', ci ? 'ci' : 'install'],
+    ['--global-style', '--loglevel=error', ci ? 'ci' : 'install'],
     {env: process.env, cwd: CONFIG.apmRootPath}
   )
 }
