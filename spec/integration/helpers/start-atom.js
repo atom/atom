21ed70687c55462df3777f97d1db7fc2d93515b2
Winston Liu <50Wliu@users.noreply.github.com>
2019-05-23 18:36:12 -0400

    WIP: Upgrade webdriverio

---
+++ spec/integration/helpers/start-atom.js
@@ -0,0 +1,155 @@
+const path = require('path')
+const http = require('http')
+const temp = require('temp').track()
+const os = require('os')
+const remote = require('remote')
+const async = require('async')
+const {map, extend, once, difference} = require('underscore-plus')
+const {spawn, spawnSync} = require('child_process')
+const webdriverio = require('../../../script/node_modules/webdriverio')
+
+const AtomPath = remote.process.argv[0]
+const AtomLauncherPath = path.join(__dirname, '..', 'helpers', 'atom-launcher.cmd')
+const ChromedriverPath = path.resolve(__dirname, '..', '..', '..', 'script', 'node_modules', 'electron-chromedriver', 'bin', 'chromedriver')
+const ChromedriverPort = 9515
+const ChromedriverURLBase = '/wd/hub'
+const ChromedriverStatusURL = `http://localhost:${ChromedriverPort}${ChromedriverURLBase}/status`
+
+let userDataDir = null
+
+const chromeDriverUp = done => {
+  const checkStatus = () =>
+    http.get(ChromedriverStatusURL, function(response) {
+      if (response.statusCode === 200) {
+        done()
+      } else {
+        chromeDriverUp(done)
+      }
+    }).on('error', () => chromeDriverUp(done))
+
+  setTimeout(checkStatus, 100)
+}
+
+const chromeDriverDown = done => {
+  const checkStatus = () =>
+    http.get(ChromedriverStatusURL, response => chromeDriverDown(done)).on('error', done)
+
+  setTimeout(checkStatus, 100)
+}
+
+const buildAtomClient = async (args, env) => {
+  userDataDir = temp.mkdirSync('atom-user-data-dir')
+  console.log('awaiting webdriverio')
+  let client
+  try {
+    client = await webdriverio.remote({
+      host: 'localhost',
+      port: ChromedriverPort,
+      capabilities: {
+        browserName: 'atom',
+        chromeOptions: {
+          binary: AtomLauncherPath,
+          args: [
+            `atom-path=${AtomPath}`,
+            `atom-args=${args.join(' ')}`,
+            `atom-env=${map(env, (value, key) => `${key}=${value}`).join(' ')}`,
+            'dev',
+            'safe',
+            `user-data-dir=${userDataDir}`
+          ]
+        }
+      }
+    })
+  } catch (error) {
+    console.log(error)
+  }
+
+  console.log('about to build client')
+
+  return client.addCommand('waitForWindowCount', async function (count, timeout) {
+    await this.waitUntil(() => this.getWindowHandles().length === count, timeout)
+    return this.getWindowHandles()
+  }).addCommand('waitForPaneItemCount', async function (count, timeout) {
+    await this.waitUntil(() => this.execute(() => {
+      if (atom.workspace) {
+        return atom.workspace.getActivePane().getItems().length
+      }
+      return 0
+    }), timeout)
+  }).addCommand('treeViewRootDirectories', async function () {
+    await $('.tree-view').waitForExist(10000)
+    return this.execute(() =>
+      Array.from(document.querySelectorAll('.tree-view .project-root > .header .name'))
+        .map(element => element.dataset.path)
+    )
+  }).addCommand('dispatchCommand', async function (command) {
+    return this.execute(async () => atom.commands.dispatch(document.activeElement, command))
+  })
+}
+
+module.exports = function(args, env, fn) {
+  let [chromedriver, chromedriverLogs, chromedriverExit] = []
+
+  // runs(() => {
+  //   chromedriver = spawn(ChromedriverPath, [
+  //     '--verbose',
+  //     `--port=${ChromedriverPort}`,
+  //     `--url-base=${ChromedriverURLBase}`
+  //   ])
+  //
+  //   chromedriverLogs = []
+  //   chromedriverExit = new Promise(resolve => {
+  //     let errorCode = null
+  //     chromedriver.on('exit', (code, signal) => {
+  //       if (signal == null) {
+  //         errorCode = code
+  //       }
+  //     })
+  //     chromedriver.stdout.on('data', log => console.log(log.toString()))
+  //     chromedriver.stderr.on('data', log => console.log(log.toString()))
+  //     // chromedriver.stderr.on('data', log => chromedriverLogs.push(log.toString()))
+  //     chromedriver.stderr.on('close', () => resolve(errorCode))
+  //   })
+  // })
+  //
+  // waitsFor('webdriver to start', chromeDriverUp, 15000)
+
+  waitsFor('tests to run', async done => {
+    const client = await buildAtomClient(args, env)
+
+    console.log('finished waiting for client')
+
+    const finish = once(async () => {
+      chromedriver.kill()
+      const errorCode = await chromedriverExit
+      if (errorCode != null) {
+        jasmine.getEnv().currentSpec.fail(`\
+Chromedriver exited with code ${errorCode}.
+Logs:\n${chromedriverLogs.join('\n')}\
+`
+        )
+      }
+      done()
+    })
+
+    // client.on('error', err => {
+    //   jasmine.getEnv().currentSpec.fail(new Error(__guard__(__guard__(err.response != null ? err.response.body : undefined, x1 => x1.value), x => x.message)))
+    //   finish()
+    // })
+
+    await client.waitUntil(() => this.getWindowHandles().length > 0, 10000)
+    await $('atom-workspace').waitForExist(10000)
+
+    console.log('about to wait on fn')
+
+    await fn(client)
+    finish()
+  }
+  , 30000)
+
+  waitsFor('webdriver to stop', chromeDriverDown, 15000)
+}
+
+function __guard__(value, transform) {
+  return (typeof value !== 'undefined' && value !== null) ? transform(value) : undefined
+}
---
+++ spec/integration/smoke-spec.js
@@ -0,0 +1,55 @@
+const fs = require('fs-plus')
+const path = require('path')
+const season = require('season')
+const temp = require('temp').track()
+const runAtom = require('./helpers/start-atom')
+
+describe('Smoke Test', () => {
+  // Fails on win32
+  if (process.platform !== 'darwin') {
+    return
+  }
+
+  const atomHome = temp.mkdirSync('atom-home')
+
+  beforeEach(() => {
+    jasmine.useRealClock()
+    season.writeFileSync(path.join(atomHome, 'config.cson'), {
+      '*': {
+        welcome: {showOnStartup: false},
+        core: {
+          telemetryConsent: 'no',
+          disabledPackages: ['github']
+        }
+      }
+    })
+  })
+
+  it('can open a file in Atom and perform basic operations on it', async () => {
+    const tempDirPath = temp.mkdirSync('empty-dir')
+    const filePath = path.join(tempDirPath, 'new-file')
+
+    fs.writeFileSync(filePath, '', {encoding: 'utf8'})
+
+    runAtom([filePath], {ATOM_HOME: atomHome}, async client => {
+      console.log('here!')
+      const roots = await client.treeViewRootDirectories()
+      expect(roots).toEqual([])
+
+      await $('atom-text-editor').waitForExist(5000)
+
+      await client.waitForPaneItemCount(1, 1000)
+
+      $('atom-text-editor').click()
+
+      await client.waitUntil(function () {
+        return this.execute(() => document.activeElement.closest('atom-text-editor'))
+      }, 5000)
+
+      const text = client.keys('Hello!').execute(() => atom.workspace.getActiveTextEditor().getText())
+      expect(text).toBe('Hello!')
+
+      await client.dispatchCommand('editor:delete-line')
+    })
+  })
+})
