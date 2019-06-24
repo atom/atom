47d9bbf93884366c48508b6216531c02c0f07f8a
Nathan Sobo <nathan@github.com>
2019-06-24 15:54:56 -0600

    Add platformRelease to data posted to Atom.io with crashes
    Co-Authored-By: Jason Rudolph <jason@jasonrudolph.com>

--- src/crash-reporter-start.js
+++ src/crash-reporter-start.js
@@ -1,11 +1,13 @@
 module.exports = function(params) {
   const { crashReporter } = require('electron');
+  const platformRelease = require('os').release();
   const { uploadToServer } = params;
 
   crashReporter.start({
     productName: 'Atom',
     companyName: 'GitHub',
     submitURL: 'https://atom.io/crash_reports',
-    uploadToServer
+    uploadToServer,
+    extra: { platformRelease }
   });
 };
