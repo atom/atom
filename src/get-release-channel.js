a582a1a188b981f8d72d25d26827c837c0ad8be9
Nathan Sobo <nathan@github.com>
2019-06-24 17:01:25 -0600

    Fix lint error

--- src/get-release-channel.js
+++ src/get-release-channel.js
@@ -1,9 +1,7 @@
 module.exports = function(version) {
   // This matches stable, dev (with or without commit hash) and any other
   // release channel following the pattern '1.00.0-channel0'
-  const match = version.match(
-    /\d+\.\d+\.\d+(-([a-z]+)(\d+|-\w{4,})?)?$/
-  );
+  const match = version.match(/\d+\.\d+\.\d+(-([a-z]+)(\d+|-\w{4,})?)?$/);
   if (!match) {
     return 'unrecognized';
   } else if (match[2]) {
@@ -11,4 +9,4 @@ module.exports = function(version) {
   }
 
   return 'stable';
-}
+};
