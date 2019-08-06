bdbb664e6a3cc5e71c973aeccc8880b95ee801c5
Jason Rudolph <jason@jasonrudolph.com>
2019-08-06 11:08:20 -0400

    Remove test that's testing a test helper
    We appreciate the thoroughness, but we think it's OK to _not_ have this 
    level of meta-testing at this point in the project.
    
    Co-authored-by: Antonio Scandurra <as-cii@github.com>

--- spec/spec-helper-spec.js
+++
@@ -1,8 +0,0 @@
-describe('spec-helper', () => {
-  describe('jasmine.any', () => {
-    it('equals uses jasmine.any', () => {
-      const func = () => {};
-      expect(func).toEqual(jasmine.any(Function));
-    });
-  });
-});
