79bc4e77af48995aad1f62bc722fcd2e493e62e2
Darangi <small_musa@hotmail.com>
2019-11-29 19:22:04 +0100

    :arrow_up: node@12.13.1 and :arrow_up:npm@6.12.1

--- script/vsts/windows-run.js
+++ script/vsts/windows-run.js
@@ -5,7 +5,7 @@ const path = require('path');
 const download = require('download');
 const childProcess = require('child_process');
 
-const nodeVersion = '10.2.1';
+const nodeVersion = '12.13.1';
 const nodeFileName = `node-v${nodeVersion}-win-x86`;
 const extractedNodePath = `c:\\tmp\\${nodeFileName}`;
 
