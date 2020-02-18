b4ce5b7260358284a1473e1095046c80317c1ab6
Darangi <small_musa@hotmail.com>
2020-02-18 22:44:42 +0100

    trigger CI

--- packages/github/test/atom/decoration.test.js
+++ packages/github/test/atom/decoration.test.js
@@ -9,7 +9,7 @@ import Marker from '../../lib/atom/marker';
 import MarkerLayer from '../../lib/atom/marker-layer';
 import ErrorBoundary from '../../lib/error-boundary';
 
-describe('Decoration', function() {
+describe('Decorations', function() {
   let atomEnv, workspace, editor, marker;
 
   beforeEach(async function() {
