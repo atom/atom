49eab7eb57d0a40b1c031485b1bc14e542999e94
Mazen El-Kashef <mazen.elkashef@gmail.com>
2020-02-26 11:31:17 -0500

    add notarization

---
+++ script/lib/notarize-on-mac.js
@@ -0,0 +1,18 @@
+const notarize = require('electron-notarize').notarize;
+
+module.exports = async function(packagedAppPath) {
+  const appBundleId = 'com.github.atom';
+  const appleId = process.env.AC_USER;
+  const appleIdPassword = process.env.AC_PASSWORD;
+  console.log(`Notarizing application at ${packagedAppPath}`);
+  try {
+    await notarize({
+      appBundleId: appBundleId,
+      appPath: packagedAppPath,
+      appleId: appleId,
+      appleIdPassword: appleIdPassword
+    });
+  } catch (e) {
+    throw new Error(e);
+  }
+};
