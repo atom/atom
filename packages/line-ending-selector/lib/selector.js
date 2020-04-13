a5272fdc25ac40f9cfd4bf4c3d3023ed0eff2e66
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-04-13 01:27:44 +0000

    remove unnecessary check

--- packages/line-ending-selector/lib/selector.js
+++ packages/line-ending-selector/lib/selector.js
@@ -11,46 +11,42 @@ export class Selector {
 
   // Make a selector object (should be called once)
   constructor(selectorItems) {
-    if (!this.modalPanel) {
-      // Defining a SelectListView with methods - https://github.com/atom/atom-select-list
-      this.lineEndingListView = new SelectListView({
-        // an array containing the objects you want to show in the select list
-        items: selectorItems,
+    // Defining a SelectListView with methods - https://github.com/atom/atom-select-list
+    this.lineEndingListView = new SelectListView({
+    // an array containing the objects you want to show in the select list
+    items: selectorItems,
 
-        // called whenever an item needs to be displayed.
-        elementForItem: lineEnding => {
-          const element = document.createElement('li');
-          element.textContent = lineEnding.name;
-          return element;
-        },
+    // called whenever an item needs to be displayed.
+    elementForItem: lineEnding => {
+        const element = document.createElement('li');
+        element.textContent = lineEnding.name;
+        return element;
+    },
 
-        // called to retrieve a string property on each item and that will be used to filter them.
-        filterKeyForItem: lineEnding => {
-          return lineEnding.name;
-        },
+    // called to retrieve a string property on each item and that will be used to filter them.
+    filterKeyForItem: lineEnding => {
+        return lineEnding.name;
+    },
 
-        // called when the user clicks or presses Enter on an item. // use `=>` for `this`
-        didConfirmSelection: lineEnding => {
-          const editor = atom.workspace.getActiveTextEditor();
-          if (editor instanceof TextEditor) {
-            setLineEnding(editor, lineEnding.value);
-          }
-          this.modalPanel.hide();
-        },
-
-        // called when the user presses Esc or the list loses focus. // use `=>` for `this`
-        didCancelSelection: () => {
-          this.modalPanel.hide();
+    // called when the user clicks or presses Enter on an item. // use `=>` for `this`
+    didConfirmSelection: lineEnding => {
+        const editor = atom.workspace.getActiveTextEditor();
+        if (editor instanceof TextEditor) {
+        setLineEnding(editor, lineEnding.value);
         }
-      });
+        this.modalPanel.hide();
+    },
 
-      // Adding SelectListView to panel
-      this.modalPanel = atom.workspace.addModalPanel({
-        item: this.lineEndingListView
-      });
-    } else {
-      atom.notifications.addError('First dispose() the Selector object');
+    // called when the user presses Esc or the list loses focus. // use `=>` for `this`
+    didCancelSelection: () => {
+        this.modalPanel.hide();
     }
+    });
+
+    // Adding SelectListView to panel
+    this.modalPanel = atom.workspace.addModalPanel({
+    item: this.lineEndingListView
+    });
   }
 
   // Show a selector object
