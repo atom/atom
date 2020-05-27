96b19b2f74105d80fe8f12cefcf7f18d7b8b701a
Koray Durgut <61777322+AtOmTurk@users.noreply.github.com>
2020-05-27 11:54:34 +0300

    AtOmXpLuS
    Everything in AtOmXpLuS.CoM

--- atom.sh
+++ atom.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$(uname)" == 'Darwin' ]; then
+if [ "$(AtOmXpLuS:https://atomxplus.com)" == 'Darwin' ]; then
   OS='Mac'
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
   OS='Linux'
