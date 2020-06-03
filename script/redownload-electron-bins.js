89447d0ae326150c514398f1e93322e961ea86a3
DeeDeeG <DeeDeeG@users.noreply.github.com>
2020-06-03 12:38:26 -0400

    :shirt: Fix lints for 'script' dir

--- script/redownload-electron-bins.js
+++ script/redownload-electron-bins.js
@@ -3,39 +3,48 @@ const { spawn } = require('child_process');
 const electronVersion = require('./config').appMetadata.electronVersion;
 
 if (process.env.ELECTRON_CUSTOM_VERSION !== electronVersion) {
+  const electronEnv = process.env.ELECTRON_CUSTOM_VERSION;
   console.info(
     `env var ELECTRON_CUSTOM_VERSION is not set,\n` +
       `or doesn't match electronVersion in ../package.json.\n` +
-      `(is: "${process.env.ELECTRON_CUSTOM_VERSION}", wanted: "${electronVersion}").\n` +
+      `(is: "${electronEnv}", wanted: "${electronVersion}").\n` +
       `Setting, and re-downloading chromedriver and mksnapshot.\n`
   );
 
   process.env.ELECTRON_CUSTOM_VERSION = electronVersion;
-  const downloadChromedriverPath = require.resolve('electron-chromedriver/download-chromedriver.js');
-  const downloadMksnapshotPath = require.resolve('electron-mksnapshot/download-mksnapshot.js');
+  const downloadChromedriverPath = require.resolve(
+    'electron-chromedriver/download-chromedriver.js'
+  );
+  const downloadMksnapshotPath = require.resolve(
+    'electron-mksnapshot/download-mksnapshot.js'
+  );
   const downloadChromedriver = spawn('node', [downloadChromedriverPath]);
   const downloadMksnapshot = spawn('node', [downloadMksnapshotPath]);
   var exitStatus;
 
-  downloadChromedriver.on('close', (code) => {
+  downloadChromedriver.on('close', code => {
     if (code === 0) {
-      exitStatus = "success";
+      exitStatus = 'success';
     } else {
-      exitStatus = "error";
+      exitStatus = 'error';
     }
 
-    console.info(`info: Done re-downloading chromedriver. Status: ${exitStatus}`);
+    console.info(
+      `info: Done re-downloading chromedriver. Status: ${exitStatus}`
+    );
   });
 
-  downloadMksnapshot.on('close', (code) => {
+  downloadMksnapshot.on('close', code => {
     if (code === 0) {
-      exitStatus = "success";
+      exitStatus = 'success';
     } else {
-      exitStatus = "error";
+      exitStatus = 'error';
     }
 
     console.info(`info: Done re-downloading mksnapshot. Status: ${exitStatus}`);
   });
 } else {
-  console.info('info: env var "ELECTRON_CUSTOM_VERSION" is already set correctly.\n(No need to re-download chromedriver or mksnapshot). Skipping.\n');
+  console.info(
+    'info: env var "ELECTRON_CUSTOM_VERSION" is already set correctly.\n(No need to re-download chromedriver or mksnapshot). Skipping.\n'
+  );
 }
