859c88b22c49844f53291b36e2524275c4be4a76
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-06-21 19:12:47 +0000

    lint

--- packages/line-ending-selector/lib/selector.js
+++ packages/line-ending-selector/lib/selector.js
@@ -14,39 +14,39 @@ export class Selector {
   constructor(selectorItems) {
     // Defining a SelectListView with methods - https://github.com/atom/atom-select-list
     this.lineEndingListView = new SelectListView({
-    // an array containing the objects you want to show in the select list
-    items: selectorItems,
+      // an array containing the objects you want to show in the select list
+      items: selectorItems,
 
-    // called whenever an item needs to be displayed.
-    elementForItem: lineEnding => {
+      // called whenever an item needs to be displayed.
+      elementForItem: lineEnding => {
         const element = document.createElement('li');
         element.textContent = lineEnding.name;
         return element;
-    },
+      },
 
-    // called to retrieve a string property on each item and that will be used to filter them.
-    filterKeyForItem: lineEnding => {
+      // called to retrieve a string property on each item and that will be used to filter them.
+      filterKeyForItem: lineEnding => {
         return lineEnding.name;
-    },
+      },
 
-    // called when the user clicks or presses Enter on an item. // use `=>` for `this`
-    didConfirmSelection: lineEnding => {
+      // called when the user clicks or presses Enter on an item. // use `=>` for `this`
+      didConfirmSelection: lineEnding => {
         const editor = atom.workspace.getActiveTextEditor();
         if (editor instanceof TextEditor) {
-        setLineEnding(editor, lineEnding.value);
+          setLineEnding(editor, lineEnding.value);
         }
         this.hide();
-    },
+      },
 
-    // called when the user presses Esc or the list loses focus. // use `=>` for `this`
-    didCancelSelection: () => {
+      // called when the user presses Esc or the list loses focus. // use `=>` for `this`
+      didCancelSelection: () => {
         this.hide();
-    }
+      }
     });
 
     // Adding SelectListView to panel
     this.modalPanel = atom.workspace.addModalPanel({
-    item: this.lineEndingListView
+      item: this.lineEndingListView
     });
   }
 
