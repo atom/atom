d77c2a20bf700173c57052ca9a7fea162c0f2893
DeeDeeG <DeeDeeG@users.noreply.github.com>
2020-08-04 14:26:19 -0400

    CI: Set appropriate default values for REPO vars
    REPO_OWNER, MAIN_REPO, and NIGHTLY_RELEASE_REPO now default to
    'atom', 'atom' and 'atom-nightly-releases', respectively.

--- script/vsts/lib/release-notes.js
+++ script/vsts/lib/release-notes.js
@@ -3,9 +3,9 @@ const octokit = require('@octokit/rest')();
 const changelog = require('pr-changelog');
 const childProcess = require('child_process');
 
-const REPO_OWNER = process.env.REPO_OWNER;
-const MAIN_REPO = process.env.MAIN_REPO;
-const NIGHTLY_RELEASE_REPO = process.env.NIGHTLY_RELEASE_REPO;
+const REPO_OWNER = process.env.REPO_OWNER ? process.env.REPO_OWNER : 'atom';
+const MAIN_REPO = process.env.MAIN_REPO ? process.env.MAIN_REPO : 'atom';
+const NIGHTLY_RELEASE_REPO = process.env.NIGHTLY_RELEASE_REPO ? process.env.NIGHTLY_RELEASE_REPO : 'atom-nightly-releases';
 
 module.exports.getRelease = async function(releaseVersion, githubToken) {
   if (githubToken) {
