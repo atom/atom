fe90f78ba72ff5184f6970fda32a2a7aa9ce29ed
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-12 14:58:57 -0500

    run installScriptDependencies in a thread

--- script/lib/install-script-dependencies.js
+++ script/lib/install-script-dependencies.js
@@ -6,11 +6,15 @@ const CONFIG = require('../config');
 
 process.env.ELECTRON_CUSTOM_VERSION = CONFIG.appMetadata.electronVersion;
 
-module.exports = function(ci) {
+function installScriptDependencies(ci) {
   console.log('Installing script dependencies');
   childProcess.execFileSync(
     CONFIG.getNpmBinPath(ci),
     ['--loglevel=error', ci ? 'ci' : 'install'],
     { env: process.env, cwd: CONFIG.scriptRootPath }
   );
-};
+}
+
+const { expose } = require(`${CONFIG.scriptRunnerModulesPath}/threads/worker`);
+expose(installScriptDependencies);
+module.exports = installScriptDependencies;
