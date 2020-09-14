66f3156457893d494119b3f2454af38d3b2814a6
Benjamin Gray <bengray178@gmail.com>
2020-09-14 23:35:19 +1000

    make bootstrap async

--- script/lib/install-apm.js
+++ script/lib/install-apm.js
@@ -4,12 +4,27 @@ const childProcess = require('child_process');
 
 const CONFIG = require('../config');
 
-module.exports = function(ci) {
+module.exports = function(ci, promise=false) {
   console.log('Installing apm');
   // npm ci leaves apm with a bunch of unmet dependencies
-  childProcess.execFileSync(
-    CONFIG.getNpmBinPath(),
-    ['--global-style', '--loglevel=error', 'install'],
-    { env: process.env, cwd: CONFIG.apmRootPath }
-  );
+
+  if (promise) {
+    return new Promise(resolve => {
+      childProcess.execFile(
+        CONFIG.getNpmBinPath(),
+        ['--global-style', '--loglevel=error', 'install'],
+        { env: process.env, cwd: CONFIG.apmRootPath },
+        () => {
+          console.log("Installed apm");
+          resolve();
+        }
+      );
+    })
+  } else {
+    childProcess.execFileSync(
+      CONFIG.getNpmBinPath(),
+      ['--global-style', '--loglevel=error', 'install'],
+      { env: process.env, cwd: CONFIG.apmRootPath }
+    );
+  }
 };
--- script/lib/install-script-dependencies.js
+++ script/lib/install-script-dependencies.js
@@ -7,10 +7,16 @@ const CONFIG = require('../config');
 process.env.ELECTRON_CUSTOM_VERSION = CONFIG.appMetadata.electronVersion;
 
 module.exports = function(ci) {
-  console.log('Installing script dependencies');
-  childProcess.execFileSync(
-    CONFIG.getNpmBinPath(ci),
-    ['--loglevel=error', ci ? 'ci' : 'install'],
-    { env: process.env, cwd: CONFIG.scriptRootPath }
-  );
+  return new Promise(resolve => {
+    console.log('Installing script dependencies');
+    childProcess.execFile(
+      CONFIG.getNpmBinPath(ci),
+      ['--loglevel=error', ci ? 'ci' : 'install'],
+      { env: process.env, cwd: CONFIG.scriptRootPath },
+      () => {
+        console.log("Installed script dependencies");
+        resolve()
+      },
+    );
+  })
 };
