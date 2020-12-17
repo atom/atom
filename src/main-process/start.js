7608c6c2edb0ff33ada2fbc70630d3e181a6067c
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-16 18:02:00 -0600

    Rename TODO to TodoElectronIssue so it can be found later

--- src/main-process/start.js
+++ src/main-process/start.js
@@ -40,7 +40,7 @@ module.exports = function start(resourcePath, devResourcePath, startTime) {
   const previousConsoleLog = console.log;
   console.log = nslog;
 
-  // TODO this should be set to true before Electron 12 - https://github.com/electron/electron/issues/18397
+  // TodoElectronIssue this should be set to true before Electron 12 - https://github.com/electron/electron/issues/18397
   app.allowRendererProcessReuse = false;
 
   app.commandLine.appendSwitch('enable-experimental-web-platform-features');
