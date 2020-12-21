301ff69060a55c153e8625ebe3addb27dad80228
sadick254 <sadickjunior@gmail.com>
2020-12-21 08:12:26 +0300

    Use nativeImage for linux icon

--- src/main-process/atom-window.js
+++ src/main-process/atom-window.js
@@ -1,4 +1,10 @@
-const { BrowserWindow, app, dialog, ipcMain } = require('electron');
+const {
+  BrowserWindow,
+  app,
+  dialog,
+  ipcMain,
+  nativeImage
+} = require('electron');
 const getAppName = require('../get-app-name');
 const path = require('path');
 const url = require('url');
@@ -59,7 +65,8 @@ module.exports = class AtomWindow extends EventEmitter {
 
     // Don't set icon on Windows so the exe's ico will be used as window and
     // taskbar's icon. See https://github.com/atom/atom/issues/4811 for more.
-    if (process.platform === 'linux') options.icon = ICON_PATH;
+    if (process.platform === 'linux')
+      options.icon = nativeImage.createFromPath(ICON_PATH);
     if (this.shouldAddCustomTitleBar()) options.titleBarStyle = 'hidden';
     if (this.shouldAddCustomInsetTitleBar())
       options.titleBarStyle = 'hiddenInset';
