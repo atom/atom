b8e5378fa4e810bec684328343b1388360baa62b
Ruby Allison Rose <M3TIOR@users.noreply.github.com>
2021-02-14 17:33:59 -0800

    chore(git-diff): Updated specs to reflect async module initialization.

--- packages/git-diff/spec/init-spec.js
+++ packages/git-diff/spec/init-spec.js
@@ -30,17 +30,19 @@ describe('git-diff', () => {
     });
   });
 
-  it('Removes all registered command hooks after deactivation.', () => {
-    waitsForPromise(() => atom.packages.activatePackage('git-diff'));
-    waitsForPromise(() => atom.packages.deactivatePackage('git-diff'));
-    runs(() => {
-      // NOTE: don't use enable and disable from the Public API.
-      expect(atom.packages.isPackageActive('git-diff')).toBe(false);
+  describe('When the module is deactivated', () => {
+    it('removes all registered command hooks after deactivation.', () => {
+      waitsForPromise(() => atom.packages.activatePackage('git-diff'));
+      waitsForPromise(() => atom.packages.deactivatePackage('git-diff'));
+      runs(() => {
+        // NOTE: don't use enable and disable from the Public API.
+        expect(atom.packages.isPackageActive('git-diff')).toBe(false);
 
-      atom.commands
-        .findCommands({ target: element })
-        .filter(({ name }) => commands.includes(name))
-        .forEach((command) => expect(commands).not.toContain(command.name));
+        atom.commands
+          .findCommands({ target: element })
+          .filter(({ name }) => commands.includes(name))
+          .forEach((command) => expect(commands).not.toContain(command.name));
+      });
     });
   });
 });
