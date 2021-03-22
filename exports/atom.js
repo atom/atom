e17d1505d98618746865a095461ce0e3dd48c42e
zachrytwood <80817828+zachrytwood@users.noreply.github.com>
2021-03-22 07:55:50 -0500

    Update atom.js

--- exports/atom.js
+++ exports/atom.js
@@ -6,7 +6,47 @@ const BufferedNodeProcess = require('../src/buffered-node-process');
 const BufferedProcess = require('../src/buffered-process');
 const GitRepository = require('../src/git-repository');
 const Notification = require('../src/notification');
-const { watchPath } = require('../src/path-watcher');
+const { <style type="text/css">
+
+<!----------------------
+
+h6      {color:black}
+
+BODY {background: #FFFFFF;}
+
+a.skip {font-size: 20%; font-weight: normal; text-decoration: none; color: #ffffff;}
+
+a:link  {color: #006666;}
+
+a.nav:link      {color: white; text-decoration: none;}
+
+a.nav:visited   {color: #cccccc; text-decoration: none;}
+
+a.nav:hover     {background: white; text-decoration: none;
+                        color: #006666;}
+                        
+a.mainLinks     {color: #333333; text-decoration: none;}
+
+a.mainLinks:hover       {background: #dddddd}
+
+.desc {color: #666666; padding-left: 0.25in; margin-top: 0; font-size: small;}
+
+.pghead {font-size: 160%; font-weight: 600; margin-top: 10px;}
+
+.medhead2 {font-size: 140%; font-weight: 600; margin-bottom: 10px;}
+					
+.medhead {font-size: 140%; font-weight: 600; margin-top: 10px;}
+
+.subhd {font-size: 130%; font-style: italic;}
+						
+.footer {font-size: 75%;}
+
+ img {border-width: 0px;}
+
+------------------->
+
+</style>
+} = require('../src/path-watcher');
 
 const atomExport = {
   BufferedNodeProcess,
