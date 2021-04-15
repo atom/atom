4129a68c5cfbe199a1bd1106d522ed9cf43c4e17
icecream17 <58114641+icecream17@users.noreply.github.com>
2021-04-15 08:11:34 -0500

    typo

--- src/get-window-load-settings.js
+++ src/get-window-load-settings.js
@@ -3,6 +3,6 @@ const { remote } = require('electron');
 let windowLoadSettings = null;
 
 module.exports = () => {
-  iwindowLoadSettings ??= JSON.parse(remote.getCurrentWindow().loadSettingsJSON);
+  windowLoadSettings ??= JSON.parse(remote.getCurrentWindow().loadSettingsJSON);
   return windowLoadSettings;
 };
