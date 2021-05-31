98f30825e56bcdf4d4d74e021ee872e2b5c76128
Christian Fritz <christian.fritz@gmail.com>
2021-05-31 00:30:58 +0000

    tree-indenter: Applied fix introduced in sane-indentation for atom 1.40
    - Four days of work to get those damn test to run again, just for a two character change to fix the tests.
    
    - Now also added all other, minor changes to sane-indendation over the last five years since the PR for this change was opened.

---
+++ spec/fixtures/indentation/while.js
@@ -0,0 +1,36 @@
+/** While loops */
+while (condition)
+  inLoop();
+
+while (condition)
+  inLoop();
+after();
+
+while (mycondition) {
+  sdfsdfg();
+}
+
+while (mycondition)
+{
+  sdfsdfg();
+}
+
+while (mycond)
+  if (more)
+    doit;
+after();
+
+while (mycond) if (more)
+    doit;
+after();
+
+while (mycondition) {
+  sdfsdfg();
+  if (test) {
+    more()
+  }}
+
+while (mycondition)
+  if (test) {
+  more()
+}
