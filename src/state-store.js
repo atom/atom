9db4ba266ff61c564d41c1fb9571f238b184ee01
steven nguyen <nguyeste008@students.garlandisd.net>
2021-06-04 08:52:54 -0500

    fix lint

--- src/state-store.js
+++ src/state-store.js
@@ -13,9 +13,9 @@ module.exports = class StateStore {
         const dbOpenRequest = indexedDB.open(this.databaseName, this.version);
         dbOpenRequest.onupgradeneeded = event => {
           let db = event.target.result;
-          db.onerror = (event) => {
+          db.onerror = event => {
             console.error('Error loading database', event);
-          }
+          };
           db.createObjectStore('states');
         };
         dbOpenRequest.onsuccess = () => {
