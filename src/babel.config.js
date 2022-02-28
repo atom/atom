fd6e2dcb97e2ed379905697836a5541d1a4dfddd
steven nguyen <nguyeste008@students.garlandisd.net>
2022-02-28 18:16:18 +0000

    fix lint
    This commit only has an effect when the script/package-lock is updated
    However, that change is already in #23668
    I've decided to wait for that pr instead of doing the same change twice
    and then having to resolve possible merge conflicts.
    And so #23654 is blocked by #23668

--- src/babel.config.js
+++ src/babel.config.js
@@ -3,7 +3,7 @@ const presets = [
     'babel-preset-atomic',
     {
       targets: {
-        electron: 11,
+        electron: 11
       },
       // some of the packages use non-strict JavaScript in ES6 modules! We need to add this for now. Eventually, we should fix those packages and remove these:
       notStrictDirectiveTriggers: ['use babel'],
@@ -15,7 +15,7 @@ const presets = [
 const plugins = [
   // Though the "loose" option was set to "false" in your @babel/preset-env config, it will not be used for @babel/plugin - proposal - private - property -in -object since the "loose" mode option was set to "true" for @babel/plugin-proposal-private-methods.
   // The "loose" option must be the same for @babel/plugin-proposal-class-properties, @babel/plugin - proposal - private - methods and @babel/plugin-proposal-private-property-in-object (when they are enabled): you can silence this warning by explicitly adding
-  ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+  ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
   // to the "plugins" section of your Babel config.
 ];
 
